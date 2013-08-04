9b7dc382663a674c2ee8d25ce3a17a1775efc4ec
adriancole <adrian.f.cole@gmail.com>
2013-08-04 08:29:29 -0700

    update github example to use feign 4.0 observable

--- examples/feign-example-cli/src/main/java/feign/example/cli/GitHubExample.java
+++ examples/feign-example-cli/src/main/java/feign/example/cli/GitHubExample.java
@@ -15,8 +15,12 @@
  */
 package feign.example.cli;
 
+import dagger.Module;
+import dagger.Provides;
 import feign.Feign;
-import feign.IncrementalCallback;
+import feign.Logger;
+import feign.Observable;
+import feign.Observer;
 import feign.RequestLine;
 import feign.gson.GsonModule;
 import java.util.List;
@@ -31,10 +35,7 @@ public class GitHubExample {
     List<Contributor> contributors(@Named("owner") String owner, @Named("repo") String repo);
 
     @RequestLine("GET /repos/{owner}/{repo}/contributors")
-    void contributors(
-        @Named("owner") String owner,
-        @Named("repo") String repo,
-        IncrementalCallback<Contributor> contributors);
+    Observable<Contributor> observable(@Named("owner") String owner, @Named("repo") String repo);
   }
 
   static class Contributor {
@@ -43,7 +44,7 @@ public class GitHubExample {
   }
 
   public static void main(String... args) throws InterruptedException {
-    GitHub github = Feign.create(GitHub.class, "https://api.github.com", new GsonModule());
+    GitHub github = Feign.create(GitHub.class, "https://api.github.com", new GitHubModule());
 
     System.out.println("Let's fetch and print a list of the contributors to this library.");
     List<Contributor> contributors = github.contributors("netflix", "feign");
@@ -51,40 +52,60 @@ public class GitHubExample {
       System.out.println(contributor.login + " (" + contributor.contributions + ")");
     }
 
-    final CountDownLatch latch = new CountDownLatch(1);
+    System.out.println("Let's treat our contributors as an observable.");
+    Observable<Contributor> observable = github.observable("netflix", "feign");
 
-    System.out.println("Now, let's do it as an incremental async task.");
-    IncrementalCallback<Contributor> task =
-        new IncrementalCallback<Contributor>() {
+    CountDownLatch latch = new CountDownLatch(2);
 
-          public int count;
-
-          // parsed directly from the text stream without an intermediate collection.
-          @Override
-          public void onNext(Contributor contributor) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
-            count++;
-          }
-
-          @Override
-          public void onSuccess() {
-            System.out.println("found " + count + " contributors");
-            latch.countDown();
-          }
-
-          @Override
-          public void onFailure(Throwable cause) {
-            cause.printStackTrace();
-            latch.countDown();
-          }
-        };
-
-    // fire a task in the background.
-    github.contributors("netflix", "feign", task);
+    System.out.println("Let's add 2 subscribers.");
+    observable.subscribe(new ContributorObserver(latch));
+    observable.subscribe(new ContributorObserver(latch));
 
     // wait for the task to complete.
     latch.await();
 
     System.exit(0);
+  }
+
+  static class ContributorObserver implements Observer<Contributor> {
+
+    private final CountDownLatch latch;
+    public int count;
+
+    public ContributorObserver(CountDownLatch latch) {
+      this.latch = latch;
+    }
+
+    // parsed directly from the text stream without an intermediate collection.
+    @Override
+    public void onNext(Contributor contributor) {
+      count++;
+    }
+
+    @Override
+    public void onSuccess() {
+      System.out.println("found " + count + " contributors");
+      latch.countDown();
+    }
+
+    @Override
+    public void onFailure(Throwable cause) {
+      cause.printStackTrace();
+      latch.countDown();
+    }
+  }
+
+  @Module(overrides = true, library = true, includes = GsonModule.class)
+  static class GitHubModule {
+
+    @Provides
+    Logger.Level loggingLevel() {
+      return Logger.Level.BASIC;
+    }
+
+    @Provides
+    Logger logger() {
+      return new Logger.ErrorLogger();
+    }
   }
 }
