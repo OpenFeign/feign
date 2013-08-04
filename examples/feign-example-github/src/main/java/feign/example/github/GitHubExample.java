5f42c147d484db15fd2f2d1231ceac701973a212
adriancole <adrian.f.cole@gmail.com>
2013-08-04 12:48:30 -0700

    renamed github example

--- examples/feign-example-github/src/main/java/feign/example/github/GitHubExample.java
+++ examples/feign-example-github/src/main/java/feign/example/github/GitHubExample.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package feign.example.cli;
+package feign.example.github;
 
 import feign.Feign;
 import feign.IncrementalCallback;
