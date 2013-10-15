c92025c60817f0874bba8fbd85f356b0389fdf37
Matt Hurne <matt@thehurnes.com>
2013-10-15 15:57:53 -0400

    Squashed commit of the following:
    commit 34eb5751c760cf1f11cdbab920d6a3a1c6f06640
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 15 15:54:20 2013 -0400
    
        Remove unnecessary defensive close of Reader
    
    commit 38e51606750517d4a52571c408190e614a4a4834
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:59:35 2013 -0400
    
        Replace wildcard import with individual imports
    
    commit cc845814ea677ba5920caf5a7a914010623caf1e
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:55:37 2013 -0400
    
        Revert GitHub example to use JacksonDecoder rather than JacksonModule now that JacksonDecoder behaves sensibly with its default ObjectMapper
    
    commit 8b9638261afe2549c3a43238ee1b66d044f969f4
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:52:45 2013 -0400
    
        Configure default ObjectMapper used by JacksonEncoder and JacksonDecoder with sensible overrides of default behaviors
    
    commit 0f275bf7574b66c20a0e6aefe0140f599638992f
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:18:26 2013 -0400
    
        Unwrap RuntimeJsonMappingExceptions caught in JacksonDecoder, since they are only ever used to wrap JsonMappingExceptions, which are IOExceptions.
    
    commit 1b6995260a5727796e388bbb0b6c88b65e182415
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:09:44 2013 -0400
    
        Update Jackson integration README
    
    commit add4007a59559e7b4e2accfa0b0a0215bab62cef
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 13:07:35 2013 -0400
    
        Update CHANGES and README to reflect addition of Jackson integration
    
    commit 86c0fcfc704b1b8d03e5eaf69c608fc2761d617b
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 12:11:56 2013 -0400
    
        Update Jackson GitHub example to make use of JacksonModule, and to avoid the need for Jackson annotations
    
    commit 1552b3f8239636da0f27ace3c7b42038536e5caf
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 12:05:56 2013 -0400
    
        Replace wildcard import with individual imports
    
    commit 0b7cfd08516dfbf66f1a69263ed456f2c0671c76
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 11:01:11 2013 -0400
    
        Initial implementation of Jackson codec
    
        This new codec may be used as an alternative to Gson.
    
    commit 94027ec3319f5145c0e18ef472d8e928e97a9527
    Author: Matt Hurne <matt@thehurnes.com>
    Date:   Tue Oct 8 08:31:14 2013 -0400
    
        Improve EncodeException and DecodeException Javadoc comments

---
+++ jackson/README.md
@@ -0,0 +1,33 @@
+Jackson Codec
+===================
+
+This module adds support for encoding and decoding JSON via Jackson.
+
+Add `JacksonEncoder` and/or `JacksonDecoder` to your `Feign.Builder` like so:
+
+```java
+GitHub github = Feign.builder()
+                     .encoder(new JacksonEncoder())
+                     .decoder(new JacksonDecoder())
+                     .target(GitHub.class, "https://api.github.com");
+```
+
+If you want to customize the `ObjectMapper` that is used, provide it to the `JacksonEncoder` and `JacksonDecoder`:
+
+```java
+ObjectMapper mapper = new ObjectMapper()
+        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        .configure(SerializationFeature.INDENT_OUTPUT, true)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+GitHub github = Feign.builder()
+                     .encoder(new JacksonEncoder(mapper))
+                     .decoder(new JacksonDecoder(mapper))
+                     .target(GitHub.class, "https://api.github.com");
+```
+
+Alternatively, you can add the encoder and decoder to your Dagger object graph using the provided `JacksonModule` like so:
+
+```java
+GitHub github = Feign.create(GitHub.class, "https://api.github.com", new JacksonModule());
+```
---
+++ jackson/src/main/java/feign/jackson/JacksonDecoder.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feign.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
+import feign.Response;
+import feign.codec.Decoder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+
+public class JacksonDecoder implements Decoder {
+  private final ObjectMapper mapper;
+
+  public JacksonDecoder() {
+    this(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+  }
+
+  public JacksonDecoder(ObjectMapper mapper) {
+    this.mapper = mapper;
+  }
+
+  @Override public Object decode(Response response, Type type) throws IOException {
+    if (response.body() == null) {
+      return null;
+    }
+    Reader reader = response.body().asReader();
+    try {
+      return mapper.readValue(reader, mapper.constructType(type));
+    } catch (RuntimeJsonMappingException e) {
+      if (e.getCause() != null && e.getCause() instanceof IOException) {
+        throw IOException.class.cast(e.getCause());
+      }
+      throw e;
+    }
+  }
+}
