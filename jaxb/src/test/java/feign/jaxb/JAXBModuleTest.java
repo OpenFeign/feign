6baa072ac1dca8145e51750a126307171063bb98
Whitaker, Greg <Greg.Whitaker@nike.com>
2014-07-16 15:04:21 -0700

    Added license to all files Removed a stray System.out Modified CHANGES.md

--- jaxb/src/test/java/feign/jaxb/JAXBModuleTest.java
+++ jaxb/src/test/java/feign/jaxb/JAXBModuleTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package feign.jaxb;
 
 import com.google.common.reflect.TypeToken;
@@ -93,8 +108,6 @@ public class JAXBModuleTest {
 
         RequestTemplate template = new RequestTemplate();
         bindings.encoder.encode(mock, template);
-
-        System.out.println(new String(template.body()));
 
         assertEquals(new String(template.body(), UTF_8), "<?xml version=\"1.0\" encoding=\"UTF-8\" " +
                 "standalone=\"yes\"?><mockObject><value>Test</value></mockObject>");
--- jaxb/src/test/java/feign/jaxb/examples/AWSSignatureVersion4.java
+++ jaxb/src/test/java/feign/jaxb/examples/AWSSignatureVersion4.java
@@ -1,22 +1,35 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package feign.jaxb.examples;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.TreeMultimap;
+import feign.Request;
+import feign.RequestTemplate;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map.Entry;
 import java.util.TimeZone;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import feign.Request;
-import feign.RequestTemplate;
 
 import static com.google.common.base.Throwables.propagate;
 import static com.google.common.collect.Iterables.transform;
