66ac24173ba01f71e5acc11b448fea47cba84ad9
Adrian Cole <acole@pivotal.io>
2016-03-14 21:29:14 +0800

    Exposes Encoder.MAP_STRING_WILDCARD to make form encoding easier
    Before, instructions around form encoding were incomplete, particularly
    around how to get a hold of a `Map<String, ?>` type. This exposes
    `Encoder.MAP_STRING_WILDCARD` to make form encoding easier.
    
    Closes #259

--- core/src/main/java/feign/Util.java
+++ core/src/main/java/feign/Util.java
@@ -68,6 +68,14 @@ public class Util {
 
   private static final int BUF_SIZE = 0x800; // 2K chars (4K bytes)
 
+  /** Type literal for {@code Map<String, ?>}. */
+  public static final Type MAP_STRING_WILDCARD =
+      new Types.ParameterizedTypeImpl(
+          null,
+          Map.class,
+          String.class,
+          new Types.WildcardTypeImpl(new Type[] {Object.class}, new Type[0]));
+
   private Util() { // no instances
   }
 
