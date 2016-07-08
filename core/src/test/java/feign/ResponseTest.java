c10522638b92db702b1b83e8055d99a83fc4cbd4
Aakash Goenka <aakash.goenka@gmail.com>
2016-07-08 04:27:40 +0100

    Lower case headers in response and use TreeMap to allow case insensitive access

--- core/src/test/java/feign/ResponseTest.java
+++ core/src/test/java/feign/ResponseTest.java
@@ -17,10 +17,15 @@ package feign;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static feign.assertj.FeignAssertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class ResponseTest {
 
@@ -31,5 +36,38 @@ public class ResponseTest {
 
     assertThat(response.reason()).isNull();
     assertThat(response.toString()).isEqualTo("HTTP/1.1 200\n\n");
+  }
+
+  @Test
+  public void lowerCasesNamesOfHeaders() {
+    Response response = Response.create(200,
+                                        null,
+                                        Collections.singletonMap("Content-Type",
+                                                                 Collections.singletonList("application/json")),
+                                        new byte[0]);
+    assertThat(response.headers()).containsOnly(entry(("content-type"), Collections.singletonList("application/json")));
+  }
+
+  @Test
+  public void canAccessHeadersCaseInsensitively() {
+    List<String> valueList = Collections.singletonList("application/json");
+    Response response = Response.create(200,
+                                        null,
+                                        Collections.singletonMap("Content-Type", valueList),
+                                        new byte[0]);
+    assertThat(response.headers().get("content-type")).isEqualTo(valueList);
+    assertThat(response.headers().get("Content-Type")).isEqualTo(valueList);
+  }
+
+  @Test
+  public void headerValuesWithSameNameOnlyVaryingInCaseAreMerged() {
+    Map<String, Collection<String>> headersMap = new LinkedHashMap<>();
+    headersMap.put("Set-Cookie", Arrays.asList("Cookie-A=Value", "Cookie-B=Value"));
+    headersMap.put("set-cookie", Arrays.asList("Cookie-C=Value"));
+
+    Response response = Response.create(200, null, headersMap, new byte[0]);
+
+    List<String> expectedHeaderValue = Arrays.asList("Cookie-A=Value", "Cookie-B=Value", "Cookie-C=Value");
+    assertThat(response.headers()).containsOnly(entry(("set-cookie"), expectedHeaderValue));
   }
 }
