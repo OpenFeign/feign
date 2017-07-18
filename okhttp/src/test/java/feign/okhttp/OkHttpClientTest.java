0193b594cd61725d444efcc7cc1a350ebb90eab5
David Tanner <darthtanner@gmail.com>
2017-07-18 02:20:43 -0600

    Add the Content-Type if specified, unless we are setting the body (#569)
    * Add the Content-Type if specified, unless we are setting the body
    
    * Update changelog

--- okhttp/src/test/java/feign/okhttp/OkHttpClientTest.java
+++ okhttp/src/test/java/feign/okhttp/OkHttpClientTest.java
@@ -16,9 +16,22 @@
 package feign.okhttp;
 
 import feign.Feign.Builder;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import feign.Response;
+import feign.Util;
+import feign.assertj.MockWebServerAssertions;
 import feign.client.AbstractClientTest;
 
 import feign.Feign;
+import okhttp3.mockwebserver.MockResponse;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 /** Tests client-specific behavior, such as ensuring Content-Length is sent when specified. */
 public class OkHttpClientTest extends AbstractClientTest {
@@ -26,5 +39,30 @@ public class OkHttpClientTest extends AbstractClientTest {
   @Override
   public Builder newBuilder() {
     return Feign.builder().client(new OkHttpClient());
+  }
+
+
+  @Test
+  public void testContentTypeWithoutCharset() throws Exception {
+    server.enqueue(new MockResponse()
+            .setBody("AAAAAAAA"));
+    OkHttpClientTestInterface api = newBuilder()
+            .target(OkHttpClientTestInterface.class, "http://localhost:" + server.getPort());
+
+    Response response = api.getWithContentType();
+    // Response length should not be null
+    assertEquals("AAAAAAAA", Util.toString(response.body().asReader()));
+
+    MockWebServerAssertions.assertThat(server.takeRequest())
+            .hasHeaders("Accept: text/plain", "Content-Type: text/plain") // Note: OkHttp adds content length.
+            .hasMethod("GET");
+  }
+
+
+  public interface OkHttpClientTestInterface {
+
+    @RequestLine("GET /")
+    @Headers({"Accept: text/plain", "Content-Type: text/plain"})
+    Response getWithContentType();
   }
 }
