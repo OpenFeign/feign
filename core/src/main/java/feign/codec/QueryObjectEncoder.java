d04748b5671752548d8947098af7442ff2a8e592
Daniel Wu <daniel.y.woo@gmail.com>
2017-12-29 17:44:37 +0800

    downgrade to jdk 1.6

--- core/src/main/java/feign/codec/QueryObjectEncoder.java
+++ core/src/main/java/feign/codec/QueryObjectEncoder.java
@@ -1,7 +1,6 @@
 package feign.codec;
 
 
-import feign.Param;
 import feign.QueryObject;
 import feign.RequestTemplate;
 
@@ -9,9 +8,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class QueryObjectEncoder implements Encoder {
 
@@ -24,7 +21,7 @@ public class QueryObjectEncoder implements Encoder {
     @Override
     public void encode(Object parametersObject, Type bodyType, RequestTemplate template) throws EncodeException {
         if (parametersObject.getClass().getAnnotation(QueryObject.class) != null) {
-            Map<String, Object> params = new HashMap<>();
+            Map<String, Object> params = new HashMap<String, Object>();
             try {
                 for (Method method : parametersObject.getClass().getMethods()) {
                     QueryObject.Param param = method.getAnnotation(QueryObject.Param.class);
@@ -32,18 +29,16 @@ public class QueryObjectEncoder implements Encoder {
                         String key = param.value();
                         Object value = method.invoke(parametersObject);
                         if (value != null) {
-                            if (List.class.isAssignableFrom(method.getReturnType())) {
-                                value = ((List<?>) value).stream().map(String::valueOf).collect(Collectors.toList());
-                            } else {
-                                value = String.valueOf(value);
-                            }
-                            params.put(key, value);
-                            template.query(key, keyToTemplate(key));
+                            value = String.valueOf(value);
                         }
+                        params.put(key, value);
+                        template.query(key, keyToTemplate(key));
                     }
                 }
                 template.resolve(params);
-            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            } catch (IllegalAccessException e) {
+                throw new EncodeException("Could not encode object query correctly", e);
+            } catch (InvocationTargetException e) {
                 throw new EncodeException("Could not encode object query correctly", e);
             }
         } else {
