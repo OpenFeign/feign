04af4065edabdaef4e9ef6ca7e1dd0f5a44b76fe
Dariusz Wawer <dwawer@pretius.com>
2018-03-22 10:17:44 +0100

    conforming code to contributing guidelines

--- core/src/main/java/feign/Util.java
+++ core/src/main/java/feign/Util.java
@@ -323,12 +323,12 @@ public class Util {
       return defaultValue;
     }
   }
-  
+
   public static String abbreviate(String text, int maxLength) {
-    if (text.length() <= maxLength) { 
-        return text;
-    } else { 
-        return text.substring(0, maxLength-2) + "..";
+    if (text.length() <= maxLength) {
+      return text;
+    } else {
+      return text.substring(0, maxLength - 2) + "..";
     }
   }
 }
