b902af8182ba0c2714b3a56b55bf358ecac31b21
Kevin Davis <kdavisk6@gmail.com>
2018-07-06 01:31:47 -0400

    Moves Java8 into Core (#732)
    * Moves Java8 into Core
    
    Moves the OptionalDecoder and StreamDecoder into Feign Core
    Removes Java8 library
    
    Closes 705
    
    * fixup! Moves Java8 into Core

--- core/src/test/java/feign/stream/StreamDecoderTest.java
+++ core/src/test/java/feign/stream/StreamDecoderTest.java
@@ -17,11 +17,9 @@ import static feign.Util.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Feign;
 import feign.RequestLine;
 import feign.Response;
-import feign.jackson.JacksonIteratorDecoder;
 import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
@@ -63,7 +61,7 @@ public class StreamDecoderTest {
           + "]\n";
 
   @Test
-  public void simpleStreamTest() throws IOException, InterruptedException {
+  public void simpleStreamTest() {
     MockWebServer server = new MockWebServer();
     server.enqueue(new MockResponse().setBody("foo\nbar"));
 
@@ -78,24 +76,6 @@ public class StreamDecoderTest {
 
     try (Stream<String> stream = api.get()) {
       assertThat(stream.collect(Collectors.toList())).isEqualTo(Arrays.asList("foo", "bar"));
-    }
-  }
-
-  @Test
-  public void simpleJsonStreamTest() throws IOException, InterruptedException {
-    MockWebServer server = new MockWebServer();
-    server.enqueue(new MockResponse().setBody(carsJson));
-
-    ObjectMapper mapper = new ObjectMapper();
-
-    StreamInterface api =
-        Feign.builder()
-            .decoder(StreamDecoder.create(JacksonIteratorDecoder.create()))
-            .doNotCloseAfterDecode()
-            .target(StreamInterface.class, server.url("/").toString());
-
-    try (Stream<StreamInterface.Car> stream = api.getCars()) {
-      assertThat(stream.collect(Collectors.toList())).hasSize(2);
     }
   }
 
@@ -126,7 +106,7 @@ public class StreamDecoderTest {
     boolean closed;
 
     @Override
-    public void close() throws IOException {
+    public void close() {
       this.closed = true;
     }
 
