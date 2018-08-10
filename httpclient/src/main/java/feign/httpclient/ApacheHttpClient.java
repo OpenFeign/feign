6d431e3a2f8002f2bd2e3852691cd1ddd579d5fe
shiya <shiya@baidu.com>
2018-08-10 13:42:00 +0800

    add charset to Response.Body when create Reader

--- httpclient/src/main/java/feign/httpclient/ApacheHttpClient.java
+++ httpclient/src/main/java/feign/httpclient/ApacheHttpClient.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2012-2018 The Feign Authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -30,6 +30,7 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -38,21 +39,24 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import feign.Client;
 import feign.Request;
 import feign.Response;
 import feign.Util;
+
 import static feign.Util.UTF_8;
 
 /**
  * This module directs Feign's http requests to Apache's
  * <a href="https://hc.apache.org/httpcomponents-client-ga/">HttpClient</a>. Ex.
- * 
+ *
  * <pre>
  * GitHub github = Feign.builder().client(new ApacheHttpClient()).target(GitHub.class,
  * "https://api.github.com");
@@ -61,173 +65,178 @@ import static feign.Util.UTF_8;
  * Based on Square, Inc's Retrofit ApacheClient implementation
  */
 public final class ApacheHttpClient implements Client {
-  private static final String ACCEPT_HEADER_NAME = "Accept";
+    private static final String ACCEPT_HEADER_NAME = "Accept";
 
-  private final HttpClient client;
+    private final HttpClient client;
 
-  public ApacheHttpClient() {
-    this(HttpClientBuilder.create().build());
-  }
-
-  public ApacheHttpClient(HttpClient client) {
-    this.client = client;
-  }
-
-  @Override
-  public Response execute(Request request, Request.Options options) throws IOException {
-    HttpUriRequest httpUriRequest;
-    try {
-      httpUriRequest = toHttpUriRequest(request, options);
-    } catch (URISyntaxException e) {
-      throw new IOException("URL '" + request.url() + "' couldn't be parsed into a URI", e);
-    }
-    HttpResponse httpResponse = client.execute(httpUriRequest);
-    return toFeignResponse(httpResponse, request);
-  }
-
-  HttpUriRequest toHttpUriRequest(Request request, Request.Options options)
-      throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
-    RequestBuilder requestBuilder = RequestBuilder.create(request.method());
-
-    // per request timeouts
-    RequestConfig requestConfig =
-        (client instanceof Configurable ? RequestConfig.copy(((Configurable) client).getConfig())
-            : RequestConfig.custom())
-                .setConnectTimeout(options.connectTimeoutMillis())
-                .setSocketTimeout(options.readTimeoutMillis())
-                .build();
-    requestBuilder.setConfig(requestConfig);
-
-    URI uri = new URIBuilder(request.url()).build();
-
-    requestBuilder.setUri(uri.getScheme() + "://" + uri.getAuthority() + uri.getRawPath());
-
-    // request query params
-    List<NameValuePair> queryParams =
-        URLEncodedUtils.parse(uri, requestBuilder.getCharset().name());
-    for (NameValuePair queryParam : queryParams) {
-      requestBuilder.addParameter(queryParam);
+    public ApacheHttpClient() {
+        this(HttpClientBuilder.create().build());
     }
 
-    // request headers
-    boolean hasAcceptHeader = false;
-    for (Map.Entry<String, Collection<String>> headerEntry : request.headers().entrySet()) {
-      String headerName = headerEntry.getKey();
-      if (headerName.equalsIgnoreCase(ACCEPT_HEADER_NAME)) {
-        hasAcceptHeader = true;
-      }
-
-      if (headerName.equalsIgnoreCase(Util.CONTENT_LENGTH)) {
-        // The 'Content-Length' header is always set by the Apache client and it
-        // doesn't like us to set it as well.
-        continue;
-      }
-
-      for (String headerValue : headerEntry.getValue()) {
-        requestBuilder.addHeader(headerName, headerValue);
-      }
-    }
-    // some servers choke on the default accept string, so we'll set it to anything
-    if (!hasAcceptHeader) {
-      requestBuilder.addHeader(ACCEPT_HEADER_NAME, "*/*");
+    public ApacheHttpClient(HttpClient client) {
+        this.client = client;
     }
 
-    // request body
-    if (request.body() != null) {
-      HttpEntity entity = null;
-      if (request.charset() != null) {
-        ContentType contentType = getContentType(request);
-        String content = new String(request.body(), request.charset());
-        entity = new StringEntity(content, contentType);
-      } else {
-        entity = new ByteArrayEntity(request.body());
-      }
-
-      requestBuilder.setEntity(entity);
-    } else {
-      requestBuilder.setEntity(new ByteArrayEntity(new byte[0]));
-    }
-
-    return requestBuilder.build();
-  }
-
-  private ContentType getContentType(Request request) {
-    ContentType contentType = ContentType.DEFAULT_TEXT;
-    for (Map.Entry<String, Collection<String>> entry : request.headers().entrySet())
-      if (entry.getKey().equalsIgnoreCase("Content-Type")) {
-        Collection<String> values = entry.getValue();
-        if (values != null && !values.isEmpty()) {
-          contentType = ContentType.parse(values.iterator().next());
-          if (contentType.getCharset() == null) {
-            contentType = contentType.withCharset(request.charset());
-          }
-          break;
+    @Override
+    public Response execute(Request request, Request.Options options) throws IOException {
+        HttpUriRequest httpUriRequest;
+        try {
+            httpUriRequest = toHttpUriRequest(request, options);
+        } catch (URISyntaxException e) {
+            throw new IOException("URL '" + request.url() + "' couldn't be parsed into a URI", e);
         }
-      }
-    return contentType;
-  }
-
-  Response toFeignResponse(HttpResponse httpResponse, Request request) throws IOException {
-    StatusLine statusLine = httpResponse.getStatusLine();
-    int statusCode = statusLine.getStatusCode();
-
-    String reason = statusLine.getReasonPhrase();
-
-    Map<String, Collection<String>> headers = new HashMap<String, Collection<String>>();
-    for (Header header : httpResponse.getAllHeaders()) {
-      String name = header.getName();
-      String value = header.getValue();
-
-      Collection<String> headerValues = headers.get(name);
-      if (headerValues == null) {
-        headerValues = new ArrayList<String>();
-        headers.put(name, headerValues);
-      }
-      headerValues.add(value);
+        HttpResponse httpResponse = client.execute(httpUriRequest);
+        return toFeignResponse(httpResponse, request);
     }
 
-    return Response.builder()
-        .status(statusCode)
-        .reason(reason)
-        .headers(headers)
-        .request(request)
-        .body(toFeignBody(httpResponse))
-        .build();
-  }
+    HttpUriRequest toHttpUriRequest(Request request, Request.Options options)
+            throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
+        RequestBuilder requestBuilder = RequestBuilder.create(request.method());
 
-  Response.Body toFeignBody(HttpResponse httpResponse) throws IOException {
-    final HttpEntity entity = httpResponse.getEntity();
-    if (entity == null) {
-      return null;
+        // per request timeouts
+        RequestConfig requestConfig =
+                (client instanceof Configurable ? RequestConfig.copy(((Configurable) client).getConfig())
+                        : RequestConfig.custom())
+                        .setConnectTimeout(options.connectTimeoutMillis())
+                        .setSocketTimeout(options.readTimeoutMillis())
+                        .build();
+        requestBuilder.setConfig(requestConfig);
+
+        URI uri = new URIBuilder(request.url()).build();
+
+        requestBuilder.setUri(uri.getScheme() + "://" + uri.getAuthority() + uri.getRawPath());
+
+        // request query params
+        List<NameValuePair> queryParams =
+                URLEncodedUtils.parse(uri, requestBuilder.getCharset().name());
+        for (NameValuePair queryParam : queryParams) {
+            requestBuilder.addParameter(queryParam);
+        }
+
+        // request headers
+        boolean hasAcceptHeader = false;
+        for (Map.Entry<String, Collection<String>> headerEntry : request.headers().entrySet()) {
+            String headerName = headerEntry.getKey();
+            if (headerName.equalsIgnoreCase(ACCEPT_HEADER_NAME)) {
+                hasAcceptHeader = true;
+            }
+
+            if (headerName.equalsIgnoreCase(Util.CONTENT_LENGTH)) {
+                // The 'Content-Length' header is always set by the Apache client and it
+                // doesn't like us to set it as well.
+                continue;
+            }
+
+            for (String headerValue : headerEntry.getValue()) {
+                requestBuilder.addHeader(headerName, headerValue);
+            }
+        }
+        // some servers choke on the default accept string, so we'll set it to anything
+        if (!hasAcceptHeader) {
+            requestBuilder.addHeader(ACCEPT_HEADER_NAME, "*/*");
+        }
+
+        // request body
+        if (request.body() != null) {
+            HttpEntity entity = null;
+            if (request.charset() != null) {
+                ContentType contentType = getContentType(request);
+                String content = new String(request.body(), request.charset());
+                entity = new StringEntity(content, contentType);
+            } else {
+                entity = new ByteArrayEntity(request.body());
+            }
+
+            requestBuilder.setEntity(entity);
+        } else {
+            requestBuilder.setEntity(new ByteArrayEntity(new byte[0]));
+        }
+
+        return requestBuilder.build();
     }
-    return new Response.Body() {
 
-      @Override
-      public Integer length() {
-        return entity.getContentLength() >= 0 && entity.getContentLength() <= Integer.MAX_VALUE
-            ? (int) entity.getContentLength()
-            : null;
-      }
+    private ContentType getContentType(Request request) {
+        ContentType contentType = ContentType.DEFAULT_TEXT;
+        for (Map.Entry<String, Collection<String>> entry : request.headers().entrySet())
+            if (entry.getKey().equalsIgnoreCase("Content-Type")) {
+                Collection<String> values = entry.getValue();
+                if (values != null && !values.isEmpty()) {
+                    contentType = ContentType.parse(values.iterator().next());
+                    if (contentType.getCharset() == null) {
+                        contentType = contentType.withCharset(request.charset());
+                    }
+                    break;
+                }
+            }
+        return contentType;
+    }
 
-      @Override
-      public boolean isRepeatable() {
-        return entity.isRepeatable();
-      }
+    Response toFeignResponse(HttpResponse httpResponse, Request request) throws IOException {
+        StatusLine statusLine = httpResponse.getStatusLine();
+        int statusCode = statusLine.getStatusCode();
 
-      @Override
-      public InputStream asInputStream() throws IOException {
-        return entity.getContent();
-      }
+        String reason = statusLine.getReasonPhrase();
 
-      @Override
-      public Reader asReader() throws IOException {
-        return new InputStreamReader(asInputStream(), UTF_8);
-      }
+        Map<String, Collection<String>> headers = new HashMap<String, Collection<String>>();
+        for (Header header : httpResponse.getAllHeaders()) {
+            String name = header.getName();
+            String value = header.getValue();
 
-      @Override
-      public void close() throws IOException {
-        EntityUtils.consume(entity);
-      }
-    };
-  }
+            Collection<String> headerValues = headers.get(name);
+            if (headerValues == null) {
+                headerValues = new ArrayList<String>();
+                headers.put(name, headerValues);
+            }
+            headerValues.add(value);
+        }
+
+        return Response.builder()
+                .status(statusCode)
+                .reason(reason)
+                .headers(headers)
+                .request(request)
+                .body(toFeignBody(httpResponse))
+                .build();
+    }
+
+    Response.Body toFeignBody(HttpResponse httpResponse) throws IOException {
+        final HttpEntity entity = httpResponse.getEntity();
+        if (entity == null) {
+            return null;
+        }
+        return new Response.Body() {
+
+            @Override
+            public Integer length() {
+                return entity.getContentLength() >= 0 && entity.getContentLength() <= Integer.MAX_VALUE
+                        ? (int) entity.getContentLength()
+                        : null;
+            }
+
+            @Override
+            public boolean isRepeatable() {
+                return entity.isRepeatable();
+            }
+
+            @Override
+            public InputStream asInputStream() throws IOException {
+                return entity.getContent();
+            }
+
+            @Override
+            public Reader asReader() throws IOException {
+                return new InputStreamReader(asInputStream(), UTF_8);
+            }
+
+            @Override
+            public Reader asReader(Charset charset) throws IOException {
+                return new InputStreamReader(asInputStream(), charset == null ? UTF_8 : charset);
+            }
+
+            @Override
+            public void close() throws IOException {
+                EntityUtils.consume(entity);
+            }
+        };
+    }
 }
