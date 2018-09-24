085f4d6eedc664a43c44dd87613b9a4d260d38db
Selim Ok <selimok@gmail.com>
2018-09-24 15:03:02 +0200

    Return null (empty target object) if status code is 204 (#792)

--- jaxb/src/main/java/feign/jaxb/JAXBDecoder.java
+++ jaxb/src/main/java/feign/jaxb/JAXBDecoder.java
@@ -65,7 +65,7 @@ public class JAXBDecoder implements Decoder {
 
   @Override
   public Object decode(Response response, Type type) throws IOException {
-    if (response.status() == 404) return Util.emptyValueOf(type);
+    if (response.status() == 404 || response.status() == 204) return Util.emptyValueOf(type);
     if (response.body() == null) return null;
     while (type instanceof ParameterizedType) {
       ParameterizedType ptype = (ParameterizedType) type;
