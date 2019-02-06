e8366d09356d88dda4968b9b0351accee1fc395c
Paul Aly <paul.aly@outlook.com>
2019-02-06 14:15:51 -0500

    Allow 1xx status codes (#871)
    * add support for 1xx status codes in Response
    
    * allow for any status code

--- core/src/test/java/feign/ResponseTest.java
+++ core/src/test/java/feign/ResponseTest.java
@@ -14,6 +14,7 @@
 package feign;
 
 import feign.Request.HttpMethod;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collection;
@@ -80,5 +81,29 @@ public class ResponseTest {
         .body(new byte[0])
         .build();
     assertThat(response.headers()).isNotNull().isEmpty();
+  }
+
+  @Test
+  public void support1xxStatusCodes() {
+    Response response = Response.builder()
+        .status(103)
+        .request(Request.create(HttpMethod.GET, "/api", Collections.emptyMap(), null, Util.UTF_8))
+        .body((Response.Body) null)
+        .build();
+
+    assertThat(response.status()).isEqualTo(103);
+  }
+
+  @Test
+  public void statusCodesOfAnyValueAreAllowed() {
+    Lists.list(600, 50, 35600).forEach(statusCode -> {
+      Response response = Response.builder()
+          .status(statusCode)
+          .request(Request.create(HttpMethod.GET, "/api", Collections.emptyMap(), null, Util.UTF_8))
+          .body((Response.Body) null)
+          .build();
+
+      assertThat(response.status()).isEqualTo(statusCode);
+    });
   }
 }
