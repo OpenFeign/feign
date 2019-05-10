9bcfd9ce149ccd0bd956f3accaa519714ea3b130
Marvin Froeder <marvin@thelastpickle.com>
2019-05-11 09:59:00 +1200

    Rename packages to cross module boundary

--- example-wikipedia/src/main/java/example/wikipedia/WikipediaExample.java
+++ example-wikipedia/src/main/java/example/wikipedia/WikipediaExample.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package feign.example.wikipedia;
+package example.wikipedia;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
