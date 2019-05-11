8b198bf3003dddeed73014be56bcde7b7a63745b
Davis, Kevin <kdavisk6@gmail.com>
2019-05-10 20:45:16 -0400

    [travis skip] prepare release 10.2.1

--- travis/release.sh
+++ travis/release.sh
@@ -30,11 +30,11 @@ echo "release version is: ${tag} and next snapshot is: ${snapshot}"
 
 # Update the versions, removing the snapshots, then create a new tag for the release, this will
 # start the travis-ci release process.
-./mvnw -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="[travis skip] prepare release ${tag}" -DpushChanges=false
+./mvnw -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="[travis skip] prepare release ${tag}"
 
 # tag the release
 echo "pushing tag ${tag}"
-./mvnw scm:tag -DpushChanges=false
+./mvnw scm:tag
 
 # Update the versions to the next snapshot
-./mvnw -B versions:set scm:checkin -DnewVersion="${snapshot}" -DgenerateBackupPoms=false -Dmessage="[travis skip] updating versions to next development iteration ${snapshot}" -DpushChanges=false
+./mvnw -B versions:set scm:checkin -DnewVersion="${snapshot}" -DgenerateBackupPoms=false -Dmessage="[travis skip] updating versions to next development iteration ${snapshot}"
