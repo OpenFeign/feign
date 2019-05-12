bf9d292871c9c56d14353a6f321f7b221b15ba7f
Marvin Froeder <velo@users.noreply.github.com>
2019-05-12 16:31:13 +1200

    Testing feign-example-github jar  (#953)
    * Running GitHubExample as unit test
    
    * Testing feign-example-github jar
    
    * Rename packages to cross module boundary
    
    * Adding github token

--- example-github/src/main/java/example/github/GitHubExample.java
+++ example-github/src/main/java/example/github/GitHubExample.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package feign.example.github;
+package example.github;
 
 import feign.Feign;
 import feign.Logger;
@@ -29,6 +29,8 @@ import java.util.stream.Collectors;
  * Inspired by {@code com.example.retrofit.GitHubClient}
  */
 public class GitHubExample {
+
+  private static final String GITHUB_TOKEN = "GITHUB_TOKEN";
 
   interface GitHub {
 
@@ -62,6 +64,14 @@ public class GitHubExample {
           .errorDecoder(new GitHubErrorDecoder(decoder))
           .logger(new Logger.ErrorLogger())
           .logLevel(Logger.Level.BASIC)
+          .requestInterceptor(template -> {
+            if (System.getenv().containsKey(GITHUB_TOKEN)) {
+              System.out.println("Detected Authorization token from environment variable");
+              template.header(
+                  "Authorization",
+                  "token " + System.getenv(GITHUB_TOKEN));
+            }
+          })
           .target(GitHub.class, "https://api.github.com");
     }
   }
@@ -105,6 +115,8 @@ public class GitHubExample {
     @Override
     public Exception decode(String methodKey, Response response) {
       try {
+        // must replace status by 200 other GSONDecoder returns null
+        response = response.toBuilder().status(200).build();
         return (Exception) decoder.decode(response, GitHubClientError.class);
       } catch (IOException fallbackToDefault) {
         return defaultDecoder.decode(methodKey, response);
--- example-wikipedia/src/main/java/example/wikipedia/ResponseAdapter.java
+++ example-wikipedia/src/main/java/example/wikipedia/ResponseAdapter.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package feign.example.wikipedia;
+package example.wikipedia;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
