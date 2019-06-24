83fc07153c92e658b5263e9a1090d5088e78aa76
David Schlosnagle <schlosna@gmail.com>
2019-06-24 18:39:41 -0400

    JacksonEncoder avoids intermediate String request body (#989)
    * JacksonEncoder avoids intermediate String request body
    
    Serialize Jackson request object directly to UTF-8 byte array without
    intermediate String representation.
    
    See https://github.com/FasterXML/jackson-docs/wiki/Presentation:-Jackson-Performance#basics-things-you-should-do-anyway
    
    * Use Util.UTF_8

--- jackson/src/main/java/feign/jackson/JacksonEncoder.java
+++ jackson/src/main/java/feign/jackson/JacksonEncoder.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import feign.RequestTemplate;
+import feign.Util;
 import feign.codec.EncodeException;
 import feign.codec.Encoder;
 import java.lang.reflect.Type;
@@ -49,7 +50,7 @@ public class JacksonEncoder implements Encoder {
   public void encode(Object object, Type bodyType, RequestTemplate template) {
     try {
       JavaType javaType = mapper.getTypeFactory().constructType(bodyType);
-      template.body(mapper.writerFor(javaType).writeValueAsString(object));
+      template.body(mapper.writerFor(javaType).writeValueAsBytes(object), Util.UTF_8);
     } catch (JsonProcessingException e) {
       throw new EncodeException(e.getMessage(), e);
     }
