88633fe0c2545d1571014d5d2fd7edff9eb4e393
Marvin Froeder <marvin@thelastpickle.com>
2019-09-04 09:21:19 +1200

    Format code

--- googlehttpclient/src/main/java/feign/googlehttpclient/GoogleHttpClient.java
+++ googlehttpclient/src/main/java/feign/googlehttpclient/GoogleHttpClient.java
@@ -23,12 +23,10 @@ import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
-
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.HashMap;
-
 import feign.Client;
 import feign.Request;
 import feign.Response;
@@ -37,92 +35,96 @@ import feign.Util;
 
 /**
  * This module directs Feign's http requests to
- * <a href="https://developers.google.com/api-client-library/java/google-http-java-client/">Google HTTP Client</a>.
+ * <a href="https://developers.google.com/api-client-library/java/google-http-java-client/">Google
+ * HTTP Client</a>.
  *
  * <pre>
  * GitHub github = Feign.builder().client(new GoogleHttpCliest()).target(GitHub.class,
  * "https://api.github.com");
  */
 public class GoogleHttpClient implements Client {
-    private final HttpTransport transport;
-    private final HttpRequestFactory requestFactory;
+  private final HttpTransport transport;
+  private final HttpRequestFactory requestFactory;
 
-    public GoogleHttpClient() {
-        this(new NetHttpTransport());
+  public GoogleHttpClient() {
+    this(new NetHttpTransport());
+  }
+
+  public GoogleHttpClient(final HttpTransport transport) {
+    this.transport = transport;
+    this.requestFactory = transport.createRequestFactory();
+  }
+
+  @Override
+  public final Response execute(final Request inputRequest,
+                                final Request.Options options)
+      throws IOException {
+    final HttpRequest request = convertRequest(inputRequest, options);
+    final HttpResponse response = request.execute();
+    return convertResponse(inputRequest, response);
+  }
+
+  private final HttpRequest convertRequest(final Request inputRequest,
+                                           final Request.Options options)
+      throws IOException {
+    // Setup the request body
+    HttpContent content = null;
+    if (inputRequest.requestBody().length() > 0) {
+      final Collection<String> contentTypeValues = inputRequest.headers().get("Content-Type");
+      String contentType = null;
+      if (contentTypeValues != null && contentTypeValues.size() > 0) {
+        contentType = contentTypeValues.iterator().next();
+      } else {
+        contentType = "application/octet-stream";
+      }
+      content = new ByteArrayContent(contentType, inputRequest.requestBody().asBytes());
     }
 
-    public GoogleHttpClient(final HttpTransport transport) {
-        this.transport = transport;
-        this.requestFactory = transport.createRequestFactory();
+    // Build the request
+    final HttpRequest request = requestFactory.buildRequest(inputRequest.httpMethod().name(),
+        new GenericUrl(inputRequest.url()),
+        content);
+    // Setup headers
+    final HttpHeaders headers = new HttpHeaders();
+    for (final Map.Entry<String, Collection<String>> header : inputRequest.headers().entrySet()) {
+      headers.set(header.getKey(), header.getValue());
     }
-
-    @Override
-    public final Response execute(final Request inputRequest,
-                                  final Request.Options options) throws IOException {
-        final HttpRequest request = convertRequest(inputRequest, options);
-        final HttpResponse response = request.execute();
-        return convertResponse(inputRequest, response);
+    // Some servers don't do well with no Accept header
+    if (inputRequest.headers().get("Accept") == null) {
+      headers.setAccept("*/*");
     }
+    request.setHeaders(headers);
 
-    private final HttpRequest convertRequest(final Request inputRequest,
-                                             final Request.Options options) throws IOException {
-        // Setup the request body
-        HttpContent content = null;
-        if (inputRequest.requestBody().length() > 0) {
-            final Collection<String> contentTypeValues = inputRequest.headers().get("Content-Type");
-            String contentType = null;
-            if (contentTypeValues != null && contentTypeValues.size() > 0) {
-                contentType = contentTypeValues.iterator().next();
-            } else {
-                contentType = "application/octet-stream";
-            }
-            content = new ByteArrayContent(contentType, inputRequest.requestBody().asBytes());
-        }
+    // Setup request options
+    request.setReadTimeout(options.readTimeoutMillis())
+        .setConnectTimeout(options.connectTimeoutMillis())
+        .setFollowRedirects(options.isFollowRedirects())
+        .setThrowExceptionOnExecuteError(false);
+    return request;
+  }
 
-        // Build the request
-        final HttpRequest request = requestFactory.buildRequest(inputRequest.httpMethod().name(),
-                                                                new GenericUrl(inputRequest.url()),
-                                                                content);
-        // Setup headers
-        final HttpHeaders headers = new HttpHeaders();
-        for (final Map.Entry<String, Collection<String>> header : inputRequest.headers().entrySet()) {
-            headers.set(header.getKey(), header.getValue());
-        }
-        // Some servers don't do well with no Accept header
-        if (inputRequest.headers().get("Accept") == null) {
-            headers.setAccept("*/*");
-        }
-        request.setHeaders(headers);
-
-        // Setup request options
-        request.setReadTimeout(options.readTimeoutMillis())
-            .setConnectTimeout(options.connectTimeoutMillis())
-            .setFollowRedirects(options.isFollowRedirects())
-            .setThrowExceptionOnExecuteError(false);
-        return request;
+  private final Response convertResponse(final Request inputRequest,
+                                         final HttpResponse inputResponse)
+      throws IOException {
+    final HttpHeaders headers = inputResponse.getHeaders();
+    Integer contentLength = null;
+    if (headers.getContentLength() != null && headers.getContentLength() <= Integer.MAX_VALUE) {
+      contentLength = inputResponse.getHeaders().getContentLength().intValue();
     }
+    return Response.builder()
+        .body(inputResponse.getContent(), contentLength)
+        .status(inputResponse.getStatusCode())
+        .reason(inputResponse.getStatusMessage())
+        .headers(toMap(inputResponse.getHeaders()))
+        .request(inputRequest)
+        .build();
+  }
 
-    private final Response convertResponse(final Request inputRequest,
-                                           final HttpResponse inputResponse) throws IOException {
-        final HttpHeaders headers = inputResponse.getHeaders();
-        Integer contentLength = null;
-        if (headers.getContentLength() != null && headers.getContentLength() <= Integer.MAX_VALUE) {
-            contentLength = inputResponse.getHeaders().getContentLength().intValue();
-        }
-        return Response.builder()
-            .body(inputResponse.getContent(), contentLength)
-            .status(inputResponse.getStatusCode())
-            .reason(inputResponse.getStatusMessage())
-            .headers(toMap(inputResponse.getHeaders()))
-            .request(inputRequest)
-            .build();
+  private final Map<String, Collection<String>> toMap(final HttpHeaders headers) {
+    final Map<String, Collection<String>> map = new HashMap<String, Collection<String>>();
+    for (final String header : headers.keySet()) {
+      map.put(header, headers.getHeaderStringValues(header));
     }
-
-    private final Map<String, Collection<String>> toMap(final HttpHeaders headers) {
-        final Map<String, Collection<String>> map = new HashMap<String, Collection<String>>();
-        for (final String header : headers.keySet()) {
-            map.put(header, headers.getHeaderStringValues(header));
-        }
-        return map;
-    }
+    return map;
+  }
 }
