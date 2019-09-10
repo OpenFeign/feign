f52158bc24bab0c6c3f858c6b07dd09d4f59dc65
Marvin Froeder <velo@users.noreply.github.com>
2019-09-11 09:04:12 +1200

    Declarative contracts (#1060)
    * Declarative contracts
    
    * Actually using the data structure to read declaritve contracts
    
    * Using declarative contract for jaxrs contracts
    
    * Make possible for contracts to declare parameters as ignored
    
    * Using predicate to decide if an AnnotationProcessor should be invoked
    
    * Restore environment variable for GITHUB_TOKEN

--- jaxrs2/src/main/java/feign/jaxrs2/JAXRS2Contract.java
+++ jaxrs2/src/main/java/feign/jaxrs2/JAXRS2Contract.java
@@ -16,23 +16,20 @@ package feign.jaxrs2;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import feign.jaxrs.JAXRSContract;
-import java.lang.annotation.Annotation;
 
 /**
  * Please refer to the <a href="https://github.com/Netflix/feign/tree/master/feign-jaxrs2">Feign
  * JAX-RS 2 README</a>.
  */
 public final class JAXRS2Contract extends JAXRSContract {
-  @Override
-  protected boolean isUnsupportedHttpParameterAnnotation(Annotation parameterAnnotation) {
-    Class<? extends Annotation> annotationType = parameterAnnotation.annotationType();
 
-    // masc20180327. parameter with unsupported jax-rs annotations should not be passed as body
-    // params.
+  public JAXRS2Contract() {
+    // parameter with unsupported jax-rs annotations should not be passed as body params.
     // this will prevent interfaces from becoming unusable entirely due to single (unsupported)
     // endpoints.
     // https://github.com/OpenFeign/feign/issues/669
-    return (annotationType == Suspended.class ||
-        annotationType == Context.class);
+    super.registerParameterAnnotation(Suspended.class, (ann, data, i) -> data.ignoreParamater(i));
+    super.registerParameterAnnotation(Context.class, (ann, data, i) -> data.ignoreParamater(i));
   }
+
 }
