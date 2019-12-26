208aca03be58ab65ca8ef11bb756620cc571dd76
Kevin Davis <kdavisk6@gmail.com>
2019-12-26 17:53:43 -0500

    Corrected formatting on new tests

--- core/src/test/java/feign/template/BodyTemplateTest.java
+++ core/src/test/java/feign/template/BodyTemplateTest.java
@@ -14,7 +14,6 @@
 package feign.template;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Collections;
 import org.junit.Test;
 
@@ -22,7 +21,8 @@ public class BodyTemplateTest {
 
   @Test
   public void bodyTemplatesSupportJsonOnlyWhenEncoded() {
-    String bodyTemplate = "%7B\"resize\": %7B\"method\": \"fit\",\"width\": {size},\"height\": {size}%7D%7D";
+    String bodyTemplate =
+        "%7B\"resize\": %7B\"method\": \"fit\",\"width\": {size},\"height\": {size}%7D%7D";
     BodyTemplate template = BodyTemplate.create(bodyTemplate);
     String expanded = template.expand(Collections.singletonMap("size", "100"));
     assertThat(expanded)
