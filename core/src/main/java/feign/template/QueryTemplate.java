25f3a9d8d48e1ce56829a40cf947f1d7eb1703fb
Kevin Davis <kdavisk6@gmail.com>
2019-12-27 14:02:01 -0500

    Include variables Query Parameter Names when listing template parameters
    Fixes #1089
    
    Query Template names, which also happen to be templates, were not being
    considered when listing out all of the variable names in a Query Template.

--- core/src/main/java/feign/template/QueryTemplate.java
+++ core/src/main/java/feign/template/QueryTemplate.java
@@ -146,7 +146,7 @@ public final class QueryTemplate {
   }
 
   public List<String> getVariables() {
-    List<String> variables = new ArrayList<>();
+    List<String> variables = new ArrayList<>(this.name.getVariables());
     for (Template template : this.values) {
       variables.addAll(template.getVariables());
     }
