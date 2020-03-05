5248f015e26882641a07361b895a9d6c7c5a6142
verils <verils@qq.com>
2020-03-05 14:18:45 +0800

    Run `mvn clean install` locally

--- googlehttpclient/src/test/java/feign/googlehttpclient/GoogleHttpClientTest.java
+++ googlehttpclient/src/test/java/feign/googlehttpclient/GoogleHttpClientTest.java
@@ -34,6 +34,5 @@ public class GoogleHttpClientTest extends AbstractClientTest {
   public void testPatch() {}
 
   @Override
-  public void parsesUnauthorizedResponseBody() {
-  }
+  public void parsesUnauthorizedResponseBody() {}
 }
