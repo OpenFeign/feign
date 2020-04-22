bd946fc67e80643e53992a92890e741b02df1e82
Kevin Davis <kdavisk6@gmail.com>
2020-04-21 22:08:03 -0400

    Correcting License Headers

--- googlehttpclient/src/test/java/feign/googlehttpclient/GoogleHttpClientTest.java
+++ googlehttpclient/src/test/java/feign/googlehttpclient/GoogleHttpClientTest.java
@@ -11,7 +11,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package feign.googlehttpclient;
 
 import feign.Feign;
