ac315eb7a3afeac28df719146bc46a07145c7173
za-PanJing <lanrong1639@126.com>
2020-12-30 20:25:57 +0800

    Add more test for (Fix bug for FeignException cannot get the correct charset) (#1325)

--- core/src/test/java/feign/FeignExceptionTest.java
+++ core/src/test/java/feign/FeignExceptionTest.java
@@ -116,24 +116,24 @@ public class FeignExceptionTest {
     map.put("connection", new ArrayList<>(Collections.singletonList("keep-alive")));
     map.put("content-length", new ArrayList<>(Collections.singletonList("100")));
     map.put("content-type",
-        new ArrayList<>(Collections.singletonList("application/json;charset=UTF-8")));
+            new ArrayList<>(Collections.singletonList("application/json;charset=UTF-8")));
 
     Request request = Request.create(Request.HttpMethod.GET,
-        "/home", Collections.emptyMap(),
-        "data".getBytes(StandardCharsets.UTF_16BE),
-        StandardCharsets.UTF_16BE,
-        null);
+            "/home", Collections.emptyMap(),
+            "data".getBytes(StandardCharsets.UTF_16BE),
+            StandardCharsets.UTF_16BE,
+            null);
 
     Response response = Response.builder()
-        .status(400)
-        .body("response".getBytes(StandardCharsets.UTF_16BE))
-        .headers(map)
-        .request(request)
-        .build();
+            .status(400)
+            .body("response".getBytes(StandardCharsets.UTF_16BE))
+            .headers(map)
+            .request(request)
+            .build();
 
     FeignException exception = FeignException.errorStatus("methodKey", response);
     assertThat(exception.getMessage())
-        .isNotEqualTo("[400] during [GET] to [/home] [methodKey]: [response]");
+            .isNotEqualTo("[400] during [GET] to [/home] [methodKey]: [response]");
   }
 
   @Test(expected = NullPointerException.class)
