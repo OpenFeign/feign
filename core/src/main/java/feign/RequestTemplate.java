e0ae518b158881a91ec2be677e07e122be290cf0
efres <evertonsegur5@gmail.com>
2021-02-02 13:40:52 -0300

    license header update year

--- core/src/main/java/feign/RequestTemplate.java
+++ core/src/main/java/feign/RequestTemplate.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012-2020 The Feign Authors
+ * Copyright 2012-2021 The Feign Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -760,10 +760,10 @@ public final class RequestTemplate implements Serializable {
    * @param headerName
    * @return true when header has that key and is not Content-Encoding
    */
-	private boolean headerIsPresentOnHeaders(String headerName) {
-		return !headerName.equalsIgnoreCase("Content-Encoding") &&
-				this.headers.keySet().stream().anyMatch(headerName::equalsIgnoreCase);
-	}
+  private boolean headerIsPresentOnHeaders(String headerName) {
+      return !headerName.equalsIgnoreCase("Content-Encoding") &&
+              this.headers.keySet().stream().anyMatch(headerName::equalsIgnoreCase);
+  }
 
 	/**
    * Headers for this Request.
