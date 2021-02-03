b79d6fcf83bc56b6ab0459c0f63688a2b6197b28
jaimepineiro <45460679+jaimepineiro@users.noreply.github.com>
2021-02-03 05:27:42 +0100

    Use charset from response Content-Type header to decode  (#1302)
    * If charset is set in Content-type, we decode response using that charset
    
    * Java 8 compatibility
    
    * Format fix
    
    * Moving code to get charset to Response in order to be used by another
    decoders

--- jackson/src/main/java/feign/jackson/JacksonDecoder.java
+++ jackson/src/main/java/feign/jackson/JacksonDecoder.java
@@ -13,15 +13,17 @@
  */
 package feign.jackson;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.Collection;
 import java.util.Collections;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import feign.Response;
 import feign.Util;
 import feign.codec.Decoder;
@@ -47,7 +49,7 @@ public class JacksonDecoder implements Decoder {
   public Object decode(Response response, Type type) throws IOException {
     if (response.body() == null)
       return null;
-    Reader reader = response.body().asReader(Util.UTF_8);
+    Reader reader = response.body().asReader(response.charset());
     if (!reader.markSupported()) {
       reader = new BufferedReader(reader, 1);
     }
@@ -66,4 +68,5 @@ public class JacksonDecoder implements Decoder {
       throw e;
     }
   }
+
 }
