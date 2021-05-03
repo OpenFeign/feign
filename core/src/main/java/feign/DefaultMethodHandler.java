c03effb3a8f7fc265c8f4244ae8bfb7ac307cf1e
Clayton Walker <clayton.m.walker@gmail.com>
2021-05-03 16:37:35 -0600

    WIP: Add support for java 9 (#1393)
    * Add support for java 9
    
    * Move multi-release to java 9 only
    
    * Have a single DefaultMethodHandle for java 8 and 11 that don't print warnings
    
    * Bump spring version
    
    Co-authored-by: Clayton Walker <cwalker@sofi.org>
    Co-authored-by: Marvin Froeder <marvin.froeder@dovetailstudios.com>

--- core/src/main/java/feign/DefaultMethodHandler.java
+++ core/src/main/java/feign/DefaultMethodHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012-2020 The Feign Authors
+ * Copyright 2012-2021 The Feign Authors
  *
  * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -15,16 +15,16 @@ package feign;
 
 import feign.InvocationHandlerFactory.MethodHandler;
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import org.jvnet.animal_sniffer.IgnoreJRERequirement;
 
 /**
  * Handles default methods by directly invoking the default method code on the interface. The bindTo
  * method must be called on the result before invoke is called.
  */
-@IgnoreJRERequirement
 final class DefaultMethodHandler implements MethodHandler {
   // Uses Java 7 MethodHandle based reflection. As default methods will only exist when
   // run on a Java 8 JVM this will not affect use on legacy JVMs.
@@ -35,18 +35,57 @@ final class DefaultMethodHandler implements MethodHandler {
   private MethodHandle handle;
 
   public DefaultMethodHandler(Method defaultMethod) {
-    try {
-      Class<?> declaringClass = defaultMethod.getDeclaringClass();
-      Field field = Lookup.class.getDeclaredField("IMPL_LOOKUP");
-      field.setAccessible(true);
-      Lookup lookup = (Lookup) field.get(null);
+    Class<?> declaringClass = defaultMethod.getDeclaringClass();
 
+    try {
+      Lookup lookup = readLookup(declaringClass);
       this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
-    } catch (NoSuchFieldException ex) {
-      throw new IllegalStateException(ex);
-    } catch (IllegalAccessException ex) {
+    } catch (NoSuchFieldException
+        | IllegalAccessException
+        | IllegalArgumentException
+        | InvocationTargetException ex) {
       throw new IllegalStateException(ex);
     }
+  }
+
+  private Lookup readLookup(Class<?> declaringClass)
+      throws IllegalAccessException, InvocationTargetException, NoSuchFieldException {
+    try {
+      return safeReadLookup(declaringClass);
+    } catch (NoSuchMethodException e) {
+      return legacyReadLookup();
+    }
+  }
+
+  /**
+   * equivalent to:
+   *
+   * <pre>
+   * return MethodHandles.privateLookupIn(declaringClass, MethodHandles.lookup());
+   * </pre>
+   *
+   * @param declaringClass
+   * @return
+   * @throws IllegalAccessException
+   * @throws InvocationTargetException
+   * @throws NoSuchMethodException
+   */
+  private Lookup safeReadLookup(Class<?> declaringClass)
+      throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    Lookup lookup = MethodHandles.lookup();
+
+    Object privateLookupIn =
+        MethodHandles.class
+            .getMethod("privateLookupIn", Class.class, Lookup.class)
+            .invoke(null, declaringClass, lookup);
+    return (Lookup) privateLookupIn;
+  }
+
+  private Lookup legacyReadLookup() throws NoSuchFieldException, IllegalAccessException {
+    Field field = Lookup.class.getDeclaredField("IMPL_LOOKUP");
+    field.setAccessible(true);
+    Lookup lookup = (Lookup) field.get(null);
+    return lookup;
   }
 
   /**
