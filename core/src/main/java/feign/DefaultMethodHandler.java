745cc03606c22483f052e34c4b7c1175cfac110e
jp <jamie@txt3.com>
2021-06-12 18:52:26 +0100

    Modify DefaultMethodHandler to work with Android.

--- core/src/main/java/feign/DefaultMethodHandler.java
+++ core/src/main/java/feign/DefaultMethodHandler.java
@@ -34,10 +34,10 @@ final class DefaultMethodHandler implements MethodHandler {
   // Uses Java 7 MethodHandle based reflection. As default methods will only exist when
   // run on a Java 8 JVM this will not affect use on legacy JVMs.
   // When Feign upgrades to Java 7, remove the @IgnoreJRERequirement annotation.
-  public MethodHandle unboundHandle;
+  private MethodHandle unboundHandle;
 
   // handle is effectively final after bindTo has been called.
-  public MethodHandle handle;
+  private MethodHandle handle;
 
   public DefaultMethodHandler(Method defaultMethod) {
     try {
