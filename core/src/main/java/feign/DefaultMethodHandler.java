d4e16a288396865564ca4157e61ee56eb72d7192
jp <jamie@txt3.com>
2021-06-14 12:59:29 +0100

    Refactor DefaultMethodHandler for Android compat.

--- core/src/main/java/feign/DefaultMethodHandler.java
+++ core/src/main/java/feign/DefaultMethodHandler.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2012-2021 The Feign Authors
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -13,7 +13,7 @@
  */
 package feign;
 
-
+import feign.InvocationHandlerFactory.MethodHandler;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
@@ -21,9 +21,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
-import feign.InvocationHandlerFactory.MethodHandler;
-
 
 /**
  * Handles default methods by directly invoking the default method code on the interface. The bindTo
@@ -33,77 +30,68 @@ final class DefaultMethodHandler implements MethodHandler {
     // Uses Java 7 MethodHandle based reflection. As default methods will only exist when
     // run on a Java 8 JVM this will not affect use on legacy JVMs.
     // When Feign upgrades to Java 7, remove the @IgnoreJRERequirement annotation.
-    public MethodHandle unboundHandle;
+    private final MethodHandle unboundHandle;
 
     // handle is effectively final after bindTo has been called.
     private MethodHandle handle;
 
-
     public DefaultMethodHandler(Method defaultMethod) {
-        /**
-         * 13/06/2021
-         *
-         * Modified to work with Android. Previous commit worked up to API level 28 (Android 8)
-         * however, Android 9+ enforces more strict checking with reflection.
-         *
-         * Solution is to use double reflection, as below.
-         *
-         * Jamie (xrpdevs-at-xrpdevs-co-uk)
-         *
-         **/
-
         Class<?> declaringClass = defaultMethod.getDeclaringClass();
 
         try {
-            if (System.getProperty("java.vm.name").equalsIgnoreCase("Dalvik")) {
-                androidObtainHandle(defaultMethod, declaringClass);
-            } else {
-                Lookup lookup = readLookup(declaringClass);
-                this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
-            }
-
-        } catch (IllegalAccessException ex) {
+            Lookup lookup = readLookup(declaringClass);
+            this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
+        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException ex) {
             throw new IllegalStateException(ex);
-        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
         }
 
-    }
-
-    public void androidObtainHandle(Method defaultMethod, Class<?> declaringClass) throws InstantiationException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        try {
-            // Android 9+ double reflection
-            Class classReference = Class.class;
-
-            Class[] classType = new Class[]{Class.class};
-
-            Method var10001 = classReference.getDeclaredMethod(
-                    "getDeclaredConstructor", Class[].class);
-
-            var10001.setAccessible(true);
-            Constructor<Lookup> someHiddenMethod =
-                    (Constructor) var10001.invoke(Lookup.class, (Object) classType);
-            Lookup lookup = someHiddenMethod.newInstance(declaringClass);
-            this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
-        } catch (IllegalAccessException ex0) {
-            // Android < 9 reflection
-            Constructor<Lookup> lookupConstructor = Lookup.class.getDeclaredConstructor(Class.class);
-            lookupConstructor.setAccessible(true);
-            Lookup lookup = lookupConstructor.newInstance(declaringClass);
-            this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
-        }
     }
 
     private Lookup readLookup(Class<?> declaringClass)
             throws IllegalAccessException, InvocationTargetException, NoSuchFieldException {
         try {
-            return safeReadLookup(declaringClass);
-        } catch (NoSuchMethodException e) {
+            if (System.getProperty("java.vm.name").equalsIgnoreCase("Dalvik")) {
+                return androidLookup(declaringClass);
+            } else {
+                return readLookup(declaringClass);
+            }
+        } catch (NoSuchMethodException | InstantiationException e) {
             return legacyReadLookup();
         }
     }
+
+    public Lookup androidLookup(Class<?> declaringClass) throws InstantiationException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Lookup lookup;
+        try {
+            // Android 9+ double reflection
+
+
+            Class classReference = Class.class;
+
+            Class[] classType = new Class[]{Class.class};
+
+            Method var10001 = classReference.getDeclaredMethod(
+                    "getDeclaredConstructor",
+                    Class[].class);
+
+            var10001.setAccessible(true);
+            Constructor<Lookup> someHiddenMethod =
+                    (Constructor) var10001.invoke(Lookup.class, (Object) classType);
+            lookup = someHiddenMethod.newInstance(declaringClass);
+            // return(lookup);
+            //aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
+        } catch (IllegalAccessException ex0) {
+            // Android < 9 reflection
+            Constructor<Lookup> lookupConstructor = Lookup.class.getDeclaredConstructor(Class.class);
+            lookupConstructor.setAccessible(true);
+            lookup = lookupConstructor.newInstance(declaringClass);
+
+            //aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
+        }
+        return(lookup);
+    }
+
 
     /**
      * equivalent to:
@@ -145,7 +133,6 @@ final class DefaultMethodHandler implements MethodHandler {
             throw new IllegalStateException(
                     "Attempted to rebind a default method handler that was already bound");
         }
-
         handle = unboundHandle.bindTo(proxy);
     }
 
