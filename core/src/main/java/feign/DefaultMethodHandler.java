89ef597e7c07a1b1c1211918fc4935a787adc482
xrpdevs <xrpdevs@xrpdevs.co.uk>
2021-06-15 11:00:28 +0100

    Refactor DefaultMethodHandler for Android compat.

--- core/src/main/java/feign/DefaultMethodHandler.java
+++ core/src/main/java/feign/DefaultMethodHandler.java
@@ -27,125 +27,126 @@ import java.lang.reflect.Method;
  * method must be called on the result before invoke is called.
  */
 final class DefaultMethodHandler implements MethodHandler {
-    // Uses Java 7 MethodHandle based reflection. As default methods will only exist when
-    // run on a Java 8 JVM this will not affect use on legacy JVMs.
-    // When Feign upgrades to Java 7, remove the @IgnoreJRERequirement annotation.
-    private final MethodHandle unboundHandle;
+  // Uses Java 7 MethodHandle based reflection. As default methods will only exist when
+  // run on a Java 8 JVM this will not affect use on legacy JVMs.
+  // When Feign upgrades to Java 7, remove the @IgnoreJRERequirement annotation.
+  private final MethodHandle unboundHandle;
 
-    // handle is effectively final after bindTo has been called.
-    private MethodHandle handle;
+  // handle is effectively final after bindTo has been called.
+  private MethodHandle handle;
 
-    public DefaultMethodHandler(Method defaultMethod) {
-        Class<?> declaringClass = defaultMethod.getDeclaringClass();
+  public DefaultMethodHandler(Method defaultMethod) {
+    Class<?> declaringClass = defaultMethod.getDeclaringClass();
 
-        try {
-            Lookup lookup = readLookup(declaringClass);
-            this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
-        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException
-                | InvocationTargetException ex) {
-            throw new IllegalStateException(ex);
-        }
-
+    try {
+      Lookup lookup = readLookup(declaringClass);
+      this.unboundHandle = lookup.unreflectSpecial(defaultMethod, declaringClass);
+    } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException
+        | InvocationTargetException ex) {
+      throw new IllegalStateException(ex);
     }
 
-    private Lookup readLookup(Class<?> declaringClass)
-            throws IllegalAccessException, InvocationTargetException, NoSuchFieldException {
-        try {
-            if (System.getProperty("java.vm.name").equalsIgnoreCase("Dalvik")) {
-                return androidLookup(declaringClass);
-            } else {
-                return safeReadLookup(declaringClass);
-            }
-        } catch (NoSuchMethodException | InstantiationException e) {
-            return legacyReadLookup();
-        }
+  }
+
+  private Lookup readLookup(Class<?> declaringClass)
+      throws IllegalAccessException, InvocationTargetException, NoSuchFieldException {
+    try {
+      if (System.getProperty("java.vm.name").equalsIgnoreCase("Dalvik")) {
+        return androidLookup(declaringClass);
+      } else {
+        return safeReadLookup(declaringClass);
+      }
+    } catch (NoSuchMethodException | InstantiationException e) {
+      return legacyReadLookup();
     }
+  }
 
-    public Lookup androidLookup(Class<?> declaringClass) throws InstantiationException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        Lookup lookup;
-        try {
-            // Android 9+ double reflection
+  public Lookup androidLookup(Class<?> declaringClass) throws InstantiationException,
+      InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    Lookup lookup;
+    try {
+      // Android 9+ double reflection
 
 
-            Class<Class> classReference = Class.class;
+      Class<Class> classReference = Class.class;
 
-            Class[] classType = new Class[]{Class.class};
+      Class[] classType = new Class[] {Class.class};
 
-            Method var10001 = classReference.getDeclaredMethod(
-                    "getDeclaredConstructor",
-                    Class[].class);
+      Method var10001 = classReference.getDeclaredMethod(
+          "getDeclaredConstructor",
+          Class[].class);
 
-            var10001.setAccessible(true);
-            Constructor someHiddenMethod =
-                    (Constructor) var10001.invoke(Lookup.class, (Object) classType);
-            lookup = (Lookup) someHiddenMethod.newInstance(declaringClass);
-            // return(lookup);
-            //aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
-        } catch (IllegalAccessException ex0) {
-            // Android < 9 reflection
-            Constructor<Lookup> lookupConstructor = Lookup.class.getDeclaredConstructor(Class.class);
-            lookupConstructor.setAccessible(true);
-            lookup = lookupConstructor.newInstance(declaringClass);
+      var10001.setAccessible(true);
+      Constructor someHiddenMethod =
+          (Constructor) var10001.invoke(Lookup.class, (Object) classType);
+      lookup = (Lookup) someHiddenMethod.newInstance(declaringClass);
+      // return(lookup);
+      // aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
+    } catch (IllegalAccessException ex0) {
+      // Android < 9 reflection
+      Constructor<Lookup> lookupConstructor = Lookup.class.getDeclaredConstructor(Class.class);
+      lookupConstructor.setAccessible(true);
+      lookup = lookupConstructor.newInstance(declaringClass);
 
-            //aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
-        }
-        return(lookup);
+      // aMH = lookup.unreflectSpecial(defaultMethod, declaringClass);
     }
+    return (lookup);
+  }
 
 
-    /**
-     * equivalent to:
-     *
-     * <pre>
-     * return MethodHandles.privateLookupIn(declaringClass, MethodHandles.lookup());
-     * </pre>
-     *
-     * @param declaringClass
-     * @return
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     * @throws NoSuchMethodException
-     */
-    private Lookup safeReadLookup(Class<?> declaringClass)
-            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        Lookup lookup = MethodHandles.lookup();
+  /**
+   * equivalent to:
+   *
+   * <pre>
+   * return MethodHandles.privateLookupIn(declaringClass, MethodHandles.lookup());
+   * </pre>
+   *
+   * @param declaringClass
+   * @return
+   * @throws IllegalAccessException
+   * @throws InvocationTargetException
+   * @throws NoSuchMethodException
+   */
+  private Lookup safeReadLookup(Class<?> declaringClass)
+      throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    Lookup lookup = MethodHandles.lookup();
 
-        Object privateLookupIn =
-                MethodHandles.class.getMethod("privateLookupIn", Class.class, Lookup.class)
-                        .invoke(null, declaringClass, lookup);
-        return (Lookup) privateLookupIn;
+    Object privateLookupIn =
+        MethodHandles.class.getMethod("privateLookupIn", Class.class, Lookup.class)
+            .invoke(null, declaringClass, lookup);
+    return (Lookup) privateLookupIn;
+  }
+
+  private Lookup legacyReadLookup() throws NoSuchFieldException, IllegalAccessException {
+    Field field = Lookup.class.getDeclaredField("IMPL_LOOKUP");
+    field.setAccessible(true);
+    Lookup lookup = (Lookup) field.get(null);
+    return lookup;
+  }
+
+  /**
+   * Bind this handler to a proxy object. After bound, DefaultMethodHandler#invoke will act as if it
+   * was called on the proxy object. Must be called once and only once for a given instance of
+   * DefaultMethodHandler
+   */
+  public void bindTo(Object proxy) {
+    if (handle != null) {
+      throw new IllegalStateException(
+          "Attempted to rebind a default method handler that was already bound");
     }
+    handle = unboundHandle.bindTo(proxy);
+  }
 
-    private Lookup legacyReadLookup() throws NoSuchFieldException, IllegalAccessException {
-        Field field = Lookup.class.getDeclaredField("IMPL_LOOKUP");
-        field.setAccessible(true);
-        Lookup lookup = (Lookup) field.get(null);
-        return lookup;
+  /**
+   * Invoke this method. DefaultMethodHandler#bindTo must be called before the first time invoke is
+   * called.
+   */
+  @Override
+  public Object invoke(Object[] argv) throws Throwable {
+    if (handle == null) {
+      throw new IllegalStateException(
+          "Default method handler invoked before proxy has been bound.");
     }
-
-    /**
-     * Bind this handler to a proxy object. After bound, DefaultMethodHandler#invoke will act as if it
-     * was called on the proxy object. Must be called once and only once for a given instance of
-     * DefaultMethodHandler
-     */
-    public void bindTo(Object proxy) {
-        if (handle != null) {
-            throw new IllegalStateException(
-                    "Attempted to rebind a default method handler that was already bound");
-        }
-        handle = unboundHandle.bindTo(proxy);
-    }
-
-    /**
-     * Invoke this method. DefaultMethodHandler#bindTo must be called before the first time invoke is
-     * called.
-     */
-    @Override
-    public Object invoke(Object[] argv) throws Throwable {
-        if (handle == null) {
-            throw new IllegalStateException(
-                    "Default method handler invoked before proxy has been bound.");
-        }
-        return handle.invokeWithArguments(argv);
-    }
+    return handle.invokeWithArguments(argv);
+  }
 }
