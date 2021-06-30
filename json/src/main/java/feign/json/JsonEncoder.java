712ff4191da48e0d859457822f047a6ecb772f7b
Witalij Berdinskich <radio_rogal@keemail.me>
2021-07-01 01:58:00 +0300

    Add JSON-java decoder and encoder (#1387)
    * Provides decoder and encoder for JSON-java
    
    * Rename feign-json to feign-org.json, add link to JSON web-site to README, use instanceof instead of references (JsonEncoder)
    
    * Return back feign-json, JSON-java
    
    * Fix parent version
    
    * Added JSON-java decoder to decoders list
    
    * Increase coverage: empty response body, parsing exception is caused by another exception
    
    * Use isAssignableFrom to allow methods return extended JSONObject and JSONArray classes
    
    Co-authored-by: Marvin Froeder <marvin.froeder@dovetailstudios.com>

---
+++ json/src/main/java/feign/json/JsonEncoder.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2012-2021 The Feign Authors
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feign.json;
+
+import static java.lang.String.format;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
+import java.lang.reflect.Type;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Encodes requests using JSON-java.
+ *
+ * <p>Basic example with {@link feign.Feign.Builder}:
+ *
+ * <pre>
+ *   interface GitHub {
+ *
+ *     {@literal @}RequestLine("POST /repos/{owner}/{repo}/contributors")
+ *     JSONObject create({@literal @}Param("owner") String owner,
+ *                       {@literal @}@Param("repo") String repo,
+ *                       JSONObject contributor);
+ *
+ *   }
+ *
+ *   GitHub github = Feign.builder()
+ *                      .decoder(new JsonDecoder())
+ *                      .encoder(new JsonEncoder())
+ *                      .target(GitHub.class, "https://api.github.com");
+ *
+ *   JSONObject contributor = new JSONObject();
+ *
+ *   contributor.put("login", "radio-rogal");
+ *   contributor.put("contributions", 0);
+ *   github.create("openfeign", "feign", contributor);
+ * </pre>
+ */
+public class JsonEncoder implements Encoder {
+
+  @Override
+  public void encode(Object object, Type bodyType, RequestTemplate template)
+      throws EncodeException {
+    if (object == null) return;
+    if (object instanceof JSONArray || object instanceof JSONObject) {
+      template.body(object.toString());
+    } else {
+      throw new EncodeException(format("%s is not a type supported by this encoder.", bodyType));
+    }
+  }
+}
---
+++ json/src/test/java/feign/json/JsonEncoderTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2012-2021 The Feign Authors
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feign.json;
+
+import static feign.Util.UTF_8;
+import static org.junit.Assert.*;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import java.util.Date;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonEncoderTest {
+
+  private JSONArray jsonArray;
+  private JSONObject jsonObject;
+  private RequestTemplate requestTemplate;
+
+  @Before
+  public void setUp() {
+    jsonObject = new JSONObject();
+    jsonObject.put("a", "b");
+    jsonObject.put("c", 1);
+    jsonArray = new JSONArray();
+    jsonArray.put(jsonObject);
+    jsonArray.put(123);
+    requestTemplate = new RequestTemplate();
+  }
+
+  @Test
+  public void encodesArray() {
+    new JsonEncoder().encode(jsonArray, JSONArray.class, requestTemplate);
+    assertEquals("[{\"a\":\"b\",\"c\":1},123]", new String(requestTemplate.body(), UTF_8));
+  }
+
+  @Test
+  public void encodesObject() {
+    new JsonEncoder().encode(jsonObject, JSONObject.class, requestTemplate);
+    assertEquals("{\"a\":\"b\",\"c\":1}", new String(requestTemplate.body(), UTF_8));
+  }
+
+  @Test
+  public void encodesNull() {
+    new JsonEncoder().encode(null, JSONObject.class, new RequestTemplate());
+    assertNull(requestTemplate.body());
+  }
+
+  @Test
+  public void unknownTypeThrowsEncodeException() {
+    Exception exception =
+        assertThrows(
+            EncodeException.class,
+            () -> new JsonEncoder().encode("qwerty", Date.class, new RequestTemplate()));
+    assertEquals(
+        "class java.util.Date is not a type supported by this encoder.", exception.getMessage());
+  }
+}
