3a351fa0716c51b5b4d7caa0c5f72cd61f80445f
Witalij Berdinskich <radio_rogal@keemail.me>
2021-10-31 22:46:09 +0200

    [java11] Add default timeout value as Request.Options uses (#1532)

--- java11/src/main/java/feign/http2client/Http2Client.java
+++ java11/src/main/java/feign/http2client/Http2Client.java
@@ -52,10 +52,21 @@ public class Http2Client implements Client, AsyncClient<Object> {
 
   private final HttpClient client;
 
+  /**
+   * Creates the new Http2Client using following defaults:
+   * <ul>
+   * <li>Connect Timeout: 10 seconds, as {@link Request.Options#Options()} uses</li>
+   * <li>Follow all 3xx redirects</li>
+   * <li>HTTP 2</li>
+   * </ul>
+   *
+   * @see Request.Options#Options()
+   */
   public Http2Client() {
     this(HttpClient.newBuilder()
         .followRedirects(Redirect.ALWAYS)
         .version(Version.HTTP_2)
+        .connectTimeout(Duration.ofMillis(10000))
         .build());
   }
 
