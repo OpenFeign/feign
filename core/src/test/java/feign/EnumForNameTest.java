df080b88e58cdd846509b93fb90a59e4c2cdc6d5
Witalij Berdinskich <radio_rogal@keemail.me>
2021-11-29 15:25:06 +0200

    [HTTP version] Fix license issue

--- core/src/test/java/feign/EnumForNameTest.java
+++ core/src/test/java/feign/EnumForNameTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2012-2021 The Feign Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package feign;
 
 import org.junit.Test;
@@ -5,9 +18,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
-
 import java.util.Arrays;
-
 import static feign.Util.enumForName;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
