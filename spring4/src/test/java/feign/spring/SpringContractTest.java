6dbc4428b94562eed9db782315df4aa7a08b3473
mroccyen <qingshanpeng@126.com>
2022-03-04 02:31:46 +0800

    SpringContract supports @RequestPart and @RequestHeader annotation parameters (#1583)

--- spring4/src/test/java/feign/spring/SpringContractTest.java
+++ spring4/src/test/java/feign/spring/SpringContractTest.java
@@ -16,6 +16,7 @@ package feign.spring;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import feign.Param;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,8 +24,7 @@ import org.junit.rules.ExpectedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.MissingResourceException;
+import java.util.*;
 import feign.Feign;
 import feign.Request;
 import feign.jackson.JacksonDecoder;
@@ -50,6 +50,10 @@ public class SpringContractTest {
         .noContent(HttpMethod.GET, "/health/1?deep=true")
         .noContent(HttpMethod.GET, "/health/1?deep=true&dryRun=true")
         .noContent(HttpMethod.GET, "/health/name?deep=true&dryRun=true")
+        .noContent(HttpMethod.POST, "/health/part/1")
+        .noContent(HttpMethod.GET, "/health/header")
+        .noContent(HttpMethod.GET, "/health/header/map")
+        .noContent(HttpMethod.GET, "/health/header/pojo")
         .ok(HttpMethod.GET, "/health/generic", "{}");
     resource = Feign.builder()
         .contract(new SpringContract())
@@ -71,6 +75,54 @@ public class SpringContractTest {
     resource.checkWithName("name", true, true);
 
     mockClient.verifyOne(HttpMethod.GET, "/health/name?deep=true&dryRun=true");
+  }
+
+  @Test
+  public void testRequestPart() {
+    resource.checkRequestPart("1", "hello", "6");
+
+    final Request request = mockClient.verifyOne(HttpMethod.POST, "/health/part/1");
+    assertThat(request.requestTemplate().methodMetadata().formParams(),
+        contains("name1", "grade1"));
+  }
+
+  @Test
+  public void testRequestHeader() {
+    resource.checkRequestHeader("hello", "6");
+
+    final Request request = mockClient.verifyOne(HttpMethod.GET, "/health/header");
+    assertThat(request.headers(),
+        hasEntry("name1", Arrays.asList("hello")));
+    assertThat(request.headers(),
+        hasEntry("grade1", Arrays.asList("6")));
+  }
+
+  @Test
+  public void testRequestHeaderMap() {
+    Map<String, String> map = new HashMap<>();
+    map.put("name1", "hello");
+    map.put("grade1", "6");
+    resource.checkRequestHeaderMap(map);
+
+    final Request request = mockClient.verifyOne(HttpMethod.GET, "/health/header/map");
+    assertThat(request.headers(),
+        hasEntry("name1", Arrays.asList("hello")));
+    assertThat(request.headers(),
+        hasEntry("grade1", Arrays.asList("6")));
+  }
+
+  @Test
+  public void testRequestHeaderPojo() {
+    HeaderMapUserObject object = new HeaderMapUserObject();
+    object.setName("hello");
+    object.setGrade("6");
+    resource.checkRequestHeaderPojo(object);
+
+    final Request request = mockClient.verifyOne(HttpMethod.GET, "/health/header/pojo");
+    assertThat(request.headers(),
+        hasEntry("name1", Arrays.asList("hello")));
+    assertThat(request.headers(),
+        hasEntry("grade1", Arrays.asList("6")));
   }
 
   @Test
@@ -151,6 +203,43 @@ public class SpringContractTest {
                        @RequestParam(name = "deep", defaultValue = "false") boolean deepCheck,
                        @RequestParam(name = "dryRun", defaultValue = "false") boolean dryRun);
 
+    @RequestMapping(value = "/part/{id}", method = RequestMethod.POST)
+    void checkRequestPart(@PathVariable(name = "id") String campaignId,
+                          @RequestPart(name = "name1") String name,
+                          @RequestPart(name = "grade1") String grade);
+
+    @RequestMapping(value = "/header", method = RequestMethod.GET)
+    void checkRequestHeader(@RequestHeader(name = "name1") String name,
+                            @RequestHeader(name = "grade1") String grade);
+
+    @RequestMapping(value = "/header/map", method = RequestMethod.GET)
+    void checkRequestHeaderMap(@RequestHeader Map<String, String> headerMap);
+
+    @RequestMapping(value = "/header/pojo", method = RequestMethod.GET)
+    void checkRequestHeaderPojo(@RequestHeader HeaderMapUserObject object);
+
   }
 
+  class HeaderMapUserObject {
+    @Param("name1")
+    private String name;
+    @Param("grade1")
+    private String grade;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getGrade() {
+      return grade;
+    }
+
+    public void setGrade(String grade) {
+      this.grade = grade;
+    }
+  }
 }
