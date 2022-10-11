6378ae2e83906bb2bcbf773a8090ff83d7e40335
Donghyeon Kim <wplong11@users.noreply.github.com>
2022-10-11 16:20:38 +0900

    Remove the unused code from the MethodInfo (#1785)
    The MethodInfo.configKey field is not more used since PR #1757 removing AsyncInvocation
    
    Co-authored-by: Marvin Froeder <velo@users.noreply.github.com>

--- core/src/test/java/feign/MethodInfoTest.java
+++ core/src/test/java/feign/MethodInfoTest.java
@@ -33,7 +33,6 @@ public class MethodInfoTest {
     @Test
     public void testCompletableFutureOfString() throws Exception {
       MethodInfo mi = new MethodInfo(AsyncClient.class, AsyncClient.class.getMethod("log"));
-      assertEquals("AsyncClient#log()", mi.configKey());
       assertTrue(mi.isAsyncReturnType());
       assertEquals(String.class, mi.underlyingReturnType());
     }
@@ -50,7 +49,6 @@ public class MethodInfoTest {
     @Test
     public void testGenericCompletableFutureOfString() throws Exception {
       MethodInfo mi = new MethodInfo(AsyncClient.class, AsyncClient.class.getMethod("log"));
-      assertEquals("AsyncClient#log()", mi.configKey());
       assertTrue(mi.isAsyncReturnType());
       assertEquals(String.class, mi.underlyingReturnType());
     }
@@ -64,7 +62,6 @@ public class MethodInfoTest {
     @Test
     public void testString() throws Exception {
       MethodInfo mi = new MethodInfo(SyncClient.class, SyncClient.class.getMethod("log"));
-      assertEquals("SyncClient#log()", mi.configKey());
       assertFalse(mi.isAsyncReturnType());
       assertEquals(String.class, mi.underlyingReturnType());
     }
@@ -98,7 +95,6 @@ public class MethodInfoTest {
     @Test
     public void testListOfStrings() throws Exception {
       MethodInfo mi = new MethodInfo(SyncClient.class, SyncClient.class.getMethod("log"));
-      assertEquals("SyncClient#log()", mi.configKey());
       assertFalse(mi.isAsyncReturnType());
       assertTrue(Types.equals(new ListOfStrings(), mi.underlyingReturnType()));
     }
