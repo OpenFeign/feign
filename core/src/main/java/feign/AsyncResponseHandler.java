2b141a00ec6d569f0b0abb4408eb4799c31c62b8
Donghyeon Kim <wplong11@users.noreply.github.com>
2022-10-12 03:52:54 +0900

    Eliminates unnecessary overhead (#1786)
    * Refactor AsyncResponseHandler
    
    * Extract ResponseHandler from AsyncResponseHandler
    
    * Use ResponseHandler on SynchronousMethodHandler
    
    Eliminates unnecessary overhead caused by CompletableFutre on synchronous operation.
    
    * Modify `AsyncResponseHandler.handleResponse` method signature
    
    * Fix code format
    
    Co-authored-by: Marvin Froeder <velo@users.noreply.github.com>
    Co-authored-by: Marvin Froeder <marvin.froeder@dovetailstudios.com>

--- core/src/main/java/feign/AsyncResponseHandler.java
+++ core/src/main/java/feign/AsyncResponseHandler.java
@@ -13,12 +13,9 @@
  */
 package feign;
 
-import static feign.FeignException.errorReading;
-import static feign.Util.ensureClosed;
 import feign.Logger.Level;
 import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.concurrent.CompletableFuture;
 
@@ -28,93 +25,40 @@ import java.util.concurrent.CompletableFuture;
  */
 @Experimental
 class AsyncResponseHandler {
-
-  private static final long MAX_RESPONSE_BUFFER_SIZE = 8192L;
-
-  private final Level logLevel;
-  private final Logger logger;
-
-  private final Decoder decoder;
-  private final ErrorDecoder errorDecoder;
-  private final boolean dismiss404;
-  private final boolean closeAfterDecode;
-
-  private final ResponseInterceptor responseInterceptor;
+  private final ResponseHandler responseHandler;
 
   AsyncResponseHandler(Level logLevel, Logger logger, Decoder decoder,
       ErrorDecoder errorDecoder, boolean dismiss404, boolean closeAfterDecode,
       ResponseInterceptor responseInterceptor) {
-    super();
-    this.logLevel = logLevel;
-    this.logger = logger;
-    this.decoder = decoder;
-    this.errorDecoder = errorDecoder;
-    this.dismiss404 = dismiss404;
-    this.closeAfterDecode = closeAfterDecode;
-    this.responseInterceptor = responseInterceptor;
+    this.responseHandler = new ResponseHandler(
+        logLevel, logger, decoder,
+        errorDecoder, dismiss404, closeAfterDecode,
+        responseInterceptor);
   }
 
-  boolean isVoidType(Type returnType) {
-    return Void.class == returnType || void.class == returnType
-        || returnType.getTypeName().equals("kotlin.Unit");
+  public CompletableFuture<Object> handleResponse(String configKey,
+                                                  Response response,
+                                                  Type returnType,
+                                                  long elapsedTime) {
+    CompletableFuture<Object> resultFuture = new CompletableFuture<>();
+    handleResponse(resultFuture, configKey, response, returnType, elapsedTime);
+    return resultFuture;
   }
 
+  /**
+   * @deprecated use {@link #handleResponse(String, Response, Type, long)} instead.
+   */
+  @Deprecated()
   void handleResponse(CompletableFuture<Object> resultFuture,
                       String configKey,
                       Response response,
                       Type returnType,
                       long elapsedTime) {
-    // copied fairly liberally from SynchronousMethodHandler
-    boolean shouldClose = true;
-
     try {
-      if (logLevel != Level.NONE) {
-        response = logger.logAndRebufferResponse(configKey, logLevel, response,
-            elapsedTime);
-      }
-      if (Response.class == returnType) {
-        if (response.body() == null) {
-          resultFuture.complete(response);
-        } else if (response.body().length() == null
-            || response.body().length() > MAX_RESPONSE_BUFFER_SIZE) {
-          shouldClose = false;
-          resultFuture.complete(response);
-        } else {
-          // Ensure the response body is disconnected
-          final byte[] bodyData = Util.toByteArray(response.body().asInputStream());
-          resultFuture.complete(response.toBuilder().body(bodyData).build());
-        }
-      } else if (response.status() >= 200 && response.status() < 300) {
-        if (isVoidType(returnType)) {
-          resultFuture.complete(null);
-        } else {
-          final Object result = decode(response, returnType);
-          shouldClose = closeAfterDecode;
-          resultFuture.complete(result);
-        }
-      } else if (dismiss404 && response.status() == 404 && !isVoidType(returnType)) {
-        final Object result = decode(response, returnType);
-        shouldClose = closeAfterDecode;
-        resultFuture.complete(result);
-      } else {
-        resultFuture.completeExceptionally(errorDecoder.decode(configKey, response));
-      }
-    } catch (final IOException e) {
-      if (logLevel != Level.NONE) {
-        logger.logIOException(configKey, logLevel, e, elapsedTime);
-      }
-      resultFuture.completeExceptionally(errorReading(response.request(), response, e));
-    } catch (final Exception e) {
+      resultFuture.complete(
+          this.responseHandler.handleResponse(configKey, response, returnType, elapsedTime));
+    } catch (Exception e) {
       resultFuture.completeExceptionally(e);
-    } finally {
-      if (shouldClose) {
-        ensureClosed(response.body());
-      }
     }
-
-  }
-
-  Object decode(Response response, Type type) throws IOException {
-    return responseInterceptor.aroundDecode(new InvocationContext(decoder, type, response));
   }
 }
