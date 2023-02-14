f6cf42b9975caa62a79855e42c19978727d8279a
Jared Komoroski <jkomoroski@alianza.com>
2023-02-13 21:20:59 -0700

    Roll Back Removal of braces

--- jaxrs/src/main/java/feign/jaxrs/JAXRSContract.java
+++ jaxrs/src/main/java/feign/jaxrs/JAXRSContract.java
@@ -117,38 +117,37 @@ public class JAXRSContract extends DeclarativeContract {
   }
 
   protected void registerParamAnnotations() {
-    registerParameterAnnotation(PathParam.class, (param, data, paramIndex) -> {
-      final String name = param.value();
-      checkState(emptyToNull(name) != null, "PathParam.value() was empty on parameter %s",
-          paramIndex);
-      nameParam(data, name, paramIndex);
-    });
-
-    registerParameterAnnotation(QueryParam.class, (param, data, paramIndex) -> {
-      final String name = param.value();
-      checkState(emptyToNull(name) != null, "QueryParam.value() was empty on parameter %s",
-          paramIndex);
-      final String query = addTemplatedParam(name);
-      data.template().query(name, query);
-      nameParam(data, name, paramIndex);
-    });
-
-    registerParameterAnnotation(HeaderParam.class, (param, data, paramIndex) -> {
-      final String name = param.value();
-      checkState(emptyToNull(name) != null, "HeaderParam.value() was empty on parameter %s",
-          paramIndex);
-      final String header = addTemplatedParam(name);
-      data.template().header(name, header);
-      nameParam(data, name, paramIndex);
-    });
-
-    registerParameterAnnotation(FormParam.class, (param, data, paramIndex) -> {
-      final String name = param.value();
-      checkState(emptyToNull(name) != null, "FormParam.value() was empty on parameter %s",
-          paramIndex);
-      data.formParams().add(name);
-      nameParam(data, name, paramIndex);
-    });
+    {
+      registerParameterAnnotation(PathParam.class, (param, data, paramIndex) -> {
+        final String name = param.value();
+        checkState(emptyToNull(name) != null, "PathParam.value() was empty on parameter %s",
+            paramIndex);
+        nameParam(data, name, paramIndex);
+      });
+      registerParameterAnnotation(QueryParam.class, (param, data, paramIndex) -> {
+        final String name = param.value();
+        checkState(emptyToNull(name) != null, "QueryParam.value() was empty on parameter %s",
+            paramIndex);
+        final String query = addTemplatedParam(name);
+        data.template().query(name, query);
+        nameParam(data, name, paramIndex);
+      });
+      registerParameterAnnotation(HeaderParam.class, (param, data, paramIndex) -> {
+        final String name = param.value();
+        checkState(emptyToNull(name) != null, "HeaderParam.value() was empty on parameter %s",
+            paramIndex);
+        final String header = addTemplatedParam(name);
+        data.template().header(name, header);
+        nameParam(data, name, paramIndex);
+      });
+      registerParameterAnnotation(FormParam.class, (param, data, paramIndex) -> {
+        final String name = param.value();
+        checkState(emptyToNull(name) != null, "FormParam.value() was empty on parameter %s",
+            paramIndex);
+        data.formParams().add(name);
+        nameParam(data, name, paramIndex);
+      });
+    }
   }
 
   // Not using override as the super-type's method is deprecated and will be removed.
