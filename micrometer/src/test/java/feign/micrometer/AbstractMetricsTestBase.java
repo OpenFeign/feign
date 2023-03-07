c2be290e648eb1a9c435b4a24c439da169f49123
rnavarropiris <rnavarropiris@users.noreply.github.com>
2023-03-07 16:18:31 +0100

    Limit line length to 100 chars

--- micrometer/src/test/java/feign/micrometer/AbstractMetricsTestBase.java
+++ micrometer/src/test/java/feign/micrometer/AbstractMetricsTestBase.java
@@ -169,7 +169,9 @@ public abstract class AbstractMetricsTestBase<MR, METRIC_ID, METRIC> {
     }
 
     assertThat(
-        getMetric("http_response_code", "http_status", "404", "status_group", "4xx", "http_method", "GET"),
+        getMetric(
+            "http_response_code", "http_status", "404", "status_group", "4xx", "http_method", "GET"
+        ),
         notNullValue());
   }
 
