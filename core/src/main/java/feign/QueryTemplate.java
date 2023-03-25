4d9bca439f9fa60db61c69ac0f18cf4514f374a0
RussiaVk <43515485+RussiaVk@users.noreply.github.com>
2023-03-25 21:50:18 +0800

    Add files via upload

---
+++ core/src/main/java/feign/QueryTemplate.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright 2012-2023 The Feign Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package feign.template;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import feign.CollectionFormat;
+import feign.Util;
+import feign.template.Template.EncodingOptions;
+import feign.template.Template.ExpansionOptions;
+
+/**
+ * Template for a Query String parameter.
+ */
+public final class QueryTemplate extends AbstractTemplate {
+
+  @SuppressWarnings("unused")
+  private static final String UNDEF = "undef";
+  private List<Template> values;
+  private final Template name;
+  private final CollectionFormat collectionFormat;
+  private boolean pure = false;
+
+  /**
+   * Create a new Query Template.
+   *
+   * @param name of the query parameter.
+   * @param values in the template.
+   * @param charset for the template.
+   * @return a QueryTemplate.
+   */
+  public static QueryTemplate create(String name, Iterable<String> values, Charset charset) {
+    return create(name, values, charset, CollectionFormat.EXPLODED, true);
+  }
+
+  public static QueryTemplate create(String name,
+                                     Iterable<String> values,
+                                     Charset charset,
+                                     CollectionFormat collectionFormat) {
+    return create(name, values, charset, collectionFormat, true);
+  }
+
+  /**
+   * Create a new Query Template.
+   *
+   * @param name of the query parameter.
+   * @param values in the template.
+   * @param charset for the template.
+   * @param collectionFormat to use.
+   * @param decodeSlash if slash characters should be decoded
+   * @return a QueryTemplate
+   */
+  public static QueryTemplate create(String name,
+                                     Iterable<String> values,
+                                     Charset charset,
+                                     CollectionFormat collectionFormat,
+                                     boolean decodeSlash) {
+    if (Util.isBlank(name)) {
+      throw new IllegalArgumentException("name is required.");
+    }
+
+    if (values == null) {
+      throw new IllegalArgumentException("values are required");
+    }
+
+    /* remove all empty values from the array */
+    Collection<String> remaining = StreamSupport.stream(values.spliterator(), false)
+        .filter(Util::isNotBlank)
+        .collect(Collectors.toList());
+
+    return new QueryTemplate(name, remaining, charset, collectionFormat, decodeSlash);
+  }
+
+  /**
+   * Append a value to the Query Template.
+   *
+   * @param queryTemplate to append to.
+   * @param values to append.
+   * @return a new QueryTemplate with value appended.
+   */
+  public static QueryTemplate append(QueryTemplate queryTemplate,
+                                     Iterable<String> values,
+                                     CollectionFormat collectionFormat,
+                                     boolean decodeSlash) {
+    List<String> queryValues = new ArrayList<>(queryTemplate.getValues());
+    queryValues.addAll(StreamSupport.stream(values.spliterator(), false)
+        .filter(Util::isNotBlank)
+        .collect(Collectors.toList()));
+    return create(queryTemplate.getName(), queryValues, StandardCharsets.UTF_8,
+        collectionFormat, decodeSlash);
+  }
+
+  /**
+   * Create a new Query Template.
+   *
+   * @param name of the query parameter.
+   * @param values for the parameter.
+   * @param collectionFormat to use.
+   */
+  private QueryTemplate(
+      String name,
+      Iterable<String> values,
+      Charset charset,
+      CollectionFormat collectionFormat,
+      boolean decodeSlash) {
+    this.values = new CopyOnWriteArrayList<>();
+    this.name = new Template(name, ExpansionOptions.ALLOW_UNRESOLVED, EncodingOptions.REQUIRED,
+        !decodeSlash, charset);
+    this.collectionFormat = collectionFormat;
+
+    /* parse each value into a template chunk for resolution later */
+    for (String value : values) {
+      if (value.isEmpty()) {
+        /* skip */
+        continue;
+      }
+
+      this.values.add(
+          new Template(
+              value,
+              ExpansionOptions.REQUIRED,
+              EncodingOptions.REQUIRED,
+              !decodeSlash,
+              charset));
+    }
+
+    if (this.values.isEmpty()) {
+      /* in this case, we have a pure parameter */
+      this.pure = true;
+
+    }
+  }
+
+  public List<String> getValues() {
+    return Collections.unmodifiableList(this.values.stream()
+        .map(Template::toString)
+        .collect(Collectors.toList()));
+  }
+
+  public List<String> getVariables() {
+    List<String> variables = new ArrayList<>(this.name.getVariables());
+    for (Template template : this.values) {
+      variables.addAll(template.getVariables());
+    }
+    return Collections.unmodifiableList(variables);
+  }
+
+  public String getName() {
+    return name.toString();
+  }
+
+  @Override
+  public String toString() {
+    return this.queryString(this.name.toString(), this.getValues());
+  }
+
+  /**
+   * Expand this template. Unresolved variables are removed. If all values remain unresolved, the
+   * result is an empty string.
+   *
+   * @param variables containing the values for expansion.
+   * @return the expanded template.
+   */
+  public String expand(Map<String, ?> variables) {
+    String name = this.name.expand(variables);
+
+    if (this.pure) {
+      return name;
+    }
+
+    List<String> expanded = new ArrayList<>();
+    for (Template template : this.values) {
+      template.setAlreadyEncoded(getAlreadyEncoded());
+      String result = template.expand(variables);
+      if (result == null) {
+        continue;
+      }
+
+      /*
+       * check for an iterable result, and if one is there, we need to split it into individual
+       * values
+       */
+      if (result.contains(",")) {
+        /* we need to split it */
+        expanded.addAll(Arrays.asList(result.split(",")));
+      } else {
+        expanded.add(result);
+      }
+    }
+
+    return this.queryString(name, Collections.unmodifiableList(expanded));
+  }
+
+
+  private String queryString(String name, List<String> values) {
+    if (this.pure) {
+      return name;
+    }
+
+    if (!values.isEmpty()) {
+      return this.collectionFormat.join(name, values, StandardCharsets.UTF_8).toString();
+    }
+
+    /* nothing to return, all values are unresolved */
+    return null;
+  }
+
+}
---
+++ core/src/main/java/feign/Template.java
@@ -0,0 +1,357 @@
+/*
+ * Copyright 2012-2023 The Feign Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package feign.template;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * A Generic representation of a Template Expression as defined by
+ * <a href="https://tools.ietf.org/html/rfc6570">RFC 6570</a>, with some relaxed rules, allowing the
+ * concept to be used in areas outside of the uri.
+ */
+public class Template extends AbstractTemplate {
+
+  private static final Logger logger = Logger.getLogger(Template.class.getName());
+  @SuppressWarnings("unused")
+  private static final Pattern QUERY_STRING_PATTERN = Pattern.compile("(?<!\\{)(\\?)");
+  private final String template;
+  private final boolean allowUnresolved;
+  private final EncodingOptions encode;
+  private final boolean encodeSlash;
+  private final Charset charset;
+  private final List<TemplateChunk> templateChunks = new ArrayList<>();
+
+
+  /**
+   * Create a new Template.
+   *
+   * @param value of the template.
+   * @param allowUnresolved if unresolved expressions should remain.
+   * @param encode all values.
+   * @param encodeSlash if slash characters should be encoded.
+   */
+  Template(
+      String value, ExpansionOptions allowUnresolved, EncodingOptions encode, boolean encodeSlash,
+      Charset charset) {
+    if (value == null) {
+      throw new IllegalArgumentException("template is required.");
+    }
+    this.template = value;
+    this.allowUnresolved = ExpansionOptions.ALLOW_UNRESOLVED == allowUnresolved;
+    this.encode = encode;
+    this.encodeSlash = encodeSlash;
+    this.charset = charset;
+    this.parseTemplate();
+  }
+
+  /**
+   * Create a new Template from the provided {@link TemplateChunk}s.
+   *
+   * @param allowUnresolved if unresolved expressions should remain.
+   * @param encode all values
+   * @param encodeSlash if slash characters should be encoded.
+   * @param charset of the result.
+   * @param chunks for this template.
+   */
+  Template(ExpansionOptions allowUnresolved, EncodingOptions encode, boolean encodeSlash,
+      Charset charset, List<TemplateChunk> chunks) {
+    this.templateChunks.addAll(chunks);
+    this.allowUnresolved = ExpansionOptions.ALLOW_UNRESOLVED == allowUnresolved;
+    this.encode = encode;
+    this.encodeSlash = encodeSlash;
+    this.charset = charset;
+    this.template = this.toString();
+  }
+
+  /**
+   * Expand the template.
+   *
+   * @param variables containing the values for expansion.
+   * @return a fully qualified URI with the variables expanded.
+   */
+  public String expand(Map<String, ?> variables) {
+    if (variables == null) {
+      throw new IllegalArgumentException("variable map is required.");
+    }
+
+    /* resolve all expressions within the template */
+    StringBuilder resolved = null;
+    for (TemplateChunk chunk : this.templateChunks) {
+
+      String expanded;
+      if (chunk instanceof Expression) {
+
+        expanded = this.resolveExpression((Expression) chunk, variables);
+
+      } else {
+        /* chunk is a literal value */
+        expanded = chunk.getValue();
+      }
+      if (expanded == null) {
+        continue;
+      }
+
+      /* append it to the result */
+      if (resolved == null) {
+        resolved = new StringBuilder();
+      }
+      resolved.append(expanded);
+    }
+
+    if (resolved == null) {
+      /* entire template is unresolved */
+      return null;
+    }
+
+    return resolved.toString();
+  }
+
+  protected String resolveExpression(
+                                     Expression expression,
+                                     Map<String, ?> variables) {
+    String resolved = null;
+    Object value = variables.get(expression.getName());
+    if (value != null) {
+      boolean isEncodingRequired = this.encode.isEncodingRequired();
+      if (getAlreadyEncoded() != null && getAlreadyEncoded().contains(expression.getName())) {
+        isEncodingRequired = false;
+      }
+
+      String expanded = expression.expand(
+          value, isEncodingRequired);
+      if (expanded != null) {
+        if (!this.encodeSlash) {
+          logger.fine("Explicit slash decoding specified, decoding all slashes in uri");
+          expanded = expanded.replaceAll("%2F", "/");
+        }
+        resolved = expanded;
+      }
+    } else {
+      if (this.allowUnresolved) {
+        /* unresolved variables are treated as literals */
+        resolved = encodeLiteral(expression.toString());
+      }
+    }
+    return resolved;
+  }
+
+  /**
+   * Uri Encode the value.
+   *
+   * @param value to encode.
+   * @return the encoded value.
+   */
+  private String encodeLiteral(String value) {
+    return this.encodeLiteral() ? UriUtils.encode(value, this.charset, true) : value;
+  }
+
+  /**
+   * Variable names contained in the template.
+   *
+   * @return a List of Variable Names.
+   */
+  public List<String> getVariables() {
+    return this.templateChunks.stream()
+        .filter(templateChunk -> Expression.class.isAssignableFrom(templateChunk.getClass()))
+        .map(templateChunk -> ((Expression) templateChunk).getName())
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * List of all Literals in the Template.
+   *
+   * @return list of Literal values.
+   */
+  public List<String> getLiterals() {
+    return this.templateChunks.stream()
+        .filter(templateChunk -> Literal.class.isAssignableFrom(templateChunk.getClass()))
+        .map(TemplateChunk::toString)
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+
+  public List<TemplateChunk> getTemplateChunks() {
+    return Collections.unmodifiableList(this.templateChunks);
+  }
+
+  /**
+   * Flag to indicate that this template is a literal string, with no variable expressions.
+   *
+   * @return true if this template is made up entirely of literal strings.
+   */
+  public boolean isLiteral() {
+    return this.getVariables().isEmpty();
+  }
+
+  /**
+   * Parse the template into {@link TemplateChunk}s.
+   */
+  private void parseTemplate() {
+
+    /* parse the entire template */
+    this.parseFragment(this.template);
+  }
+
+  /**
+   * Parse a template fragment.
+   *
+   * @param fragment to parse
+   */
+  private void parseFragment(String fragment) {
+    ChunkTokenizer tokenizer = new ChunkTokenizer(fragment);
+
+    while (tokenizer.hasNext()) {
+      /* check to see if we have an expression or a literal */
+      String chunk = tokenizer.next();
+
+      if (chunk.startsWith("{")) {
+        Expression expression = Expressions.create(chunk);
+        if (expression == null) {
+          this.templateChunks.add(Literal.create(this.encodeLiteral(chunk)));
+        } else {
+          this.templateChunks.add(expression);
+        }
+      } else {
+        this.templateChunks.add(Literal.create(this.encodeLiteral(chunk)));
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    return this.templateChunks.stream()
+        .map(TemplateChunk::getValue).collect(Collectors.joining());
+  }
+
+  public boolean encodeLiteral() {
+    return encode.isEncodingRequired();
+  }
+
+  boolean encodeSlash() {
+    return encodeSlash;
+  }
+
+  /**
+   * The Charset for the template.
+   *
+   * @return the Charset, if set. Defaults to UTF-8
+   */
+  public Charset getCharset() {
+    return this.charset;
+  }
+
+  /**
+   * Splits a Uri into Chunks that exists inside and outside of an expression, delimited by curly
+   * braces "{}". Nested expressions are treated as literals, for example "foo{bar{baz}}" will be
+   * treated as "foo, {bar{baz}}". Inspired by Apache CXF Jax-RS.
+   */
+  static class ChunkTokenizer {
+
+    private List<String> tokens = new ArrayList<>();
+    private int index;
+
+    ChunkTokenizer(String template) {
+      boolean outside = true;
+      int level = 0;
+      int lastIndex = 0;
+      int idx;
+
+      /* loop through the template, character by character */
+      for (idx = 0; idx < template.length(); idx++) {
+        if (template.charAt(idx) == '{') {
+          /* start of an expression */
+          if (outside) {
+            /* outside of an expression */
+            if (lastIndex < idx) {
+              /* this is the start of a new token */
+              tokens.add(template.substring(lastIndex, idx));
+            }
+            lastIndex = idx;
+
+            /*
+             * no longer outside of an expression, additional characters will be treated as in an
+             * expression
+             */
+            outside = false;
+          } else {
+            /* nested braces, increase our nesting level */
+            level++;
+          }
+        } else if (template.charAt(idx) == '}' && !outside) {
+          /* the end of an expression */
+          if (level > 0) {
+            /*
+             * sometimes we see nested expressions, we only want the outer most expression
+             * boundaries.
+             */
+            level--;
+          } else {
+            /* outermost boundary */
+            if (lastIndex < idx) {
+              /* this is the end of an expression token */
+              tokens.add(template.substring(lastIndex, idx + 1));
+            }
+            lastIndex = idx + 1;
+
+            /* outside an expression */
+            outside = true;
+          }
+        }
+      }
+      if (lastIndex < idx) {
+        /* grab the remaining chunk */
+        tokens.add(template.substring(lastIndex, idx));
+      }
+    }
+
+    public boolean hasNext() {
+      return this.tokens.size() > this.index;
+    }
+
+    public String next() {
+      if (hasNext()) {
+        return this.tokens.get(this.index++);
+      }
+      throw new IllegalStateException("No More Elements");
+    }
+  }
+
+  public enum EncodingOptions {
+    REQUIRED(true), NOT_REQUIRED(false);
+
+    private final boolean shouldEncode;
+
+    EncodingOptions(boolean shouldEncode) {
+      this.shouldEncode = shouldEncode;
+    }
+
+    public boolean isEncodingRequired() {
+      return this.shouldEncode;
+    }
+  }
+
+  public enum ExpansionOptions {
+    ALLOW_UNRESOLVED, REQUIRED
+  }
+
+}
