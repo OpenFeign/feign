178ac2725d1bc7b40a81e0ad381f0b5e76d62baf
RussiaVk <43515485+RussiaVk@users.noreply.github.com>
2023-03-26 10:51:18 +0800

    Add license header

--- core/src/test/java/feign/utils/RecordInvokeUtilsTest.java
+++ core/src/test/java/feign/utils/RecordInvokeUtilsTest.java
@@ -1,11 +1,22 @@
+/*
+ * Copyright 2012-2023 The Feign Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package feign.utils;
 
-import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.Array;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,7 +29,8 @@ public final class RecordInvokeUtilsTest {
 
   private record TestRecord(
                             @RecordComponentTest("value1") String name,
-                            @RecordComponentTest("value2") int age) {}
+                            @RecordComponentTest("value2") int age) {
+  }
 
   @Test
   public void isRecord() {
