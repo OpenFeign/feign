1940dc8f28915888ab08b91ddc3f601fb0ede20b
RussiaVk <43515485+RussiaVk@users.noreply.github.com>
2023-03-26 11:17:10 +0800

    Add license header

--- jakarta/src/main/java16/jakarta/ws/rs/DefaultValue.java
+++ jakarta/src/main/java16/jakarta/ws/rs/DefaultValue.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2010, 2019 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v. 2.0, which is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary Licenses when the
+ * conditions for such availability set forth in the Eclipse Public License v. 2.0 are satisfied:
+ * GNU General Public License, version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package jakarta.ws.rs;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Defines the default value of request meta-data that is bound using one of the following
  * annotations: {@link jakarta.ws.rs.PathParam}, {@link jakarta.ws.rs.QueryParam},
@@ -25,17 +47,8 @@
  * @see CookieParam
  * @since 1.0
  */
-package jakarta.ws.rs;
-
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-
 @Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD,
-  ElementType.RECORD_COMPONENT})
+    ElementType.RECORD_COMPONENT})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface DefaultValue {
