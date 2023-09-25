4f0470a75e54bcb1bf3eedf99755d964b9248a8d
= <sangakarthikraj1999@gmail.com>
2023-09-25 16:36:29 -0500

    added dummy comment

--- core/src/main/java/feign/utils/ExceptionUtils.java
+++ core/src/main/java/feign/utils/ExceptionUtils.java
@@ -32,6 +32,7 @@ public class ExceptionUtils {
       return null;
     }
     Throwable rootCause = throwable;
+    //Karthik made a comment
     // this is to avoid infinite loops for recursive cases
     final Set<Throwable> seenThrowables = new HashSet<>();
     seenThrowables.add(rootCause);
