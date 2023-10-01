624c99578f29ef8b24abfe16b4c679e6ac8e2616
Tanish <tanishazad03@gmail.com>
2023-10-01 09:48:03 +0530

    removed testVeryLongResponseNullLength

--- jaxrs2/src/test/java/feign/jaxrs2/JAXRSClientTest.java
+++ jaxrs2/src/test/java/feign/jaxrs2/JAXRSClientTest.java
@@ -176,9 +176,4 @@ public class JAXRSClientTest extends AbstractClientTest {
     Response consumesMultipleWithContentTypeHeaderAndBody(@HeaderParam("Content-Type") String contentType,
                                                           String body);
   }
-
-  @Override
-  public void testVeryLongResponseNullLength() {
-    assumeFalse("JaxRS client hang if the response doesn't have a payload", false);
-  }
 }
