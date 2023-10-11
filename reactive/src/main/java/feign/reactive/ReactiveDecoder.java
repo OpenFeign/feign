bffdd7e598d26d7e861d8d98f196a02607c7d695
Sergey Korneev <23792@mail.ru>
2023-10-11 16:01:41 +0200

    Update code as per suggestions

--- reactive/src/main/java/feign/reactive/ReactiveDecoder.java
+++ reactive/src/main/java/feign/reactive/ReactiveDecoder.java
@@ -16,6 +16,7 @@ package feign.reactive;
 import feign.FeignException;
 import feign.Response;
 import feign.Types;
+import feign.codec.DecodeException;
 import feign.codec.Decoder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -37,12 +38,18 @@ public class ReactiveDecoder implements Decoder {
         Class<?> rawType = Types.getRawType(type);
         if (rawType.isAssignableFrom(Mono.class)) {
             Type lastType = Types.resolveLastTypeParameter(type, Mono.class);
-            return Mono.just(delegate.decode(response, lastType));
+            return Mono.fromCallable(() -> delegate.decode(response, lastType));
         }
         if (rawType.isAssignableFrom(Flux.class)) {
             Type lastType = Types.resolveLastTypeParameter(type, Flux.class);
             Type listType = Types.parameterize(List.class, lastType);
-            return Flux.fromIterable((Iterable)delegate.decode(response, listType));
+            Object decoded = delegate.decode(response, listType);
+            if (decoded instanceof Iterable) {
+                return Flux.fromIterable((Iterable) decoded);
+            } else {
+                String errorMessage = "Expected type Iterable, but was: " + decoded.getClass();
+                throw new DecodeException(response.status(), errorMessage, response.request());
+            }
         }
 
         return delegate.decode(response, type);
