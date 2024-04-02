bbdda58a1825df7e96081f70fe3e3bfd1e0f3fe1
Pasqual Koschmieder <pasqual.koschmieder@gmail.com>
2024-04-02 11:28:45 +0200

    update restricted headers in jdk11 client implementation (#2366)
    * update restricted headers in jdk11 client implementation
    
    * update copyright header

--- java11/src/main/java/feign/http2client/Http2Client.java
+++ java11/src/main/java/feign/http2client/Http2Client.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 The Feign Authors
+ * Copyright 2012-2024 The Feign Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -204,19 +204,7 @@ public class Http2Client implements Client, AsyncClient<Object> {
   static {
     // A case insensitive TreeSet of strings.
     final TreeSet<String> treeSet = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-    treeSet.addAll(
-        Set.of(
-            "connection",
-            "content-length",
-            "date",
-            "expect",
-            "from",
-            "host",
-            "origin",
-            "referer",
-            "upgrade",
-            "via",
-            "warning"));
+    treeSet.addAll(Set.of("connection", "content-length", "expect", "host", "upgrade"));
     DISALLOWED_HEADERS_SET = Collections.unmodifiableSet(treeSet);
   }
 
