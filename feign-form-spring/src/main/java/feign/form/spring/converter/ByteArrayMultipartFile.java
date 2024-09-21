f68977eccfd883e92c6bf83b11714bff9ac625e4
Marvin Froeder <velo.br@gmail.com>
2024-09-21 01:13:09 -0300

    Format sources and fix build

--- feign-form-spring/src/main/java/feign/form/spring/converter/ByteArrayMultipartFile.java
+++ feign-form-spring/src/main/java/feign/form/spring/converter/ByteArrayMultipartFile.java
@@ -1,19 +1,16 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright 2012-2024 The Feign Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package feign.form.spring.converter;
 
 import java.io.ByteArrayInputStream;
@@ -23,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import lombok.NonNull;
 import lombok.Value;
-import lombok.val;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -58,7 +54,7 @@ class ByteArrayMultipartFile implements MultipartFile {
 
   @Override
   public void transferTo(File destination) throws IOException {
-    try (val outputStream = new FileOutputStream(destination)) {
+    try (var outputStream = new FileOutputStream(destination)) {
       outputStream.write(bytes);
     }
   }
--- feign-form/src/main/java/feign/form/util/PojoUtil.java
+++ feign-form/src/main/java/feign/form/util/PojoUtil.java
@@ -1,19 +1,16 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright 2012-2024 The Feign Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package feign.form.util;
 
 import static java.lang.reflect.Modifier.isFinal;
@@ -27,7 +24,6 @@ import java.rmi.UnexpectedException;
 import java.security.PrivilegedAction;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Nullable;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
@@ -54,7 +50,6 @@ public final class PojoUtil {
   }
 
   @SneakyThrows
-  @SuppressFBWarnings("DP_DO_INSIDE_DO_PRIVILEGED")
   public static Map<String, Object> toMap(@NonNull Object object) {
     val result = new HashMap<String, Object>();
     val type = object.getClass();
@@ -89,7 +84,7 @@ public final class PojoUtil {
   @FieldDefaults(level = PRIVATE)
   private static final class SetAccessibleAction implements PrivilegedAction<Object> {
 
-    @Nullable Field field;
+    Field field;
 
     @Override
     public Object run() {
