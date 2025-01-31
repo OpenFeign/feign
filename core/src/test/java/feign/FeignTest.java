ee37b00a74ff940bb5bb087ffb2a19ced1a98064
kevin <kevin@trumpetinc.com>
2025-01-31 09:43:33 -0700

    Added streaming retry tests

--- core/src/test/java/feign/FeignTest.java
+++ core/src/test/java/feign/FeignTest.java
@@ -31,6 +31,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -1255,6 +1256,70 @@ public class FeignTest {
 	    assertThat(result).isEqualTo(expectedResponse);
   }
   
+  Feign.Builder createRetryableFeignBuilder(){
+	  return Feign.builder()
+		.encoder(new InputStreamAndFileEncoder(null))
+      .decoder(
+              new StringDecoder() {
+                @Override
+                public Object decode(Response response, Type type) throws IOException {
+                  String string = super.decode(response, type).toString();
+                  if ("retry!".equals(string)) {
+                    throw new RetryableException(
+                        response.status(),
+                        string,
+                        HttpMethod.POST,
+                        NON_RETRYABLE,
+                        response.request());
+                  }
+                  return string;
+                }
+              });
+  }
+  
+  @Test
+  void streamingRequestCanRetry() throws Exception {
+	  
+    server.enqueue(new MockResponse().setBody("retry!"));
+    server.enqueue(new MockResponse().setBody("success!"));
+
+    LargeStreamTestInterface api = createRetryableFeignBuilder()
+        	.target( LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+    
+    byte[] requestData = new byte[16184];
+	new Random().nextBytes(requestData);
+    
+	// if we use a plain ByteArrayInputStream, then it is infinitely resettable, so the following will retry 
+	// if instead we wrapped this in a reset limited inputstream (like a BufferedInputStream with size set to 1024), retry would fail.
+	// As of now, I don't see a way to tell the server to read *some* of the request and then fail, so we aren't getting good test coverage of the partial request reset scenario
+    String rslt = api.postLargeStream(new ByteArrayInputStream(requestData));
+    byte[] dataReceivedByServer = server.takeRequest().getBody().readByteArray();
+    
+    assertThat(rslt).isEqualTo("success!");
+    assertThat(server.getRequestCount()).isEqualTo(2);
+    assertThat(dataReceivedByServer).isEqualTo(requestData);
+  }  
+  
+  @Test
+  void streamingRequestRetryFailsIfTooMuchDataRead() throws Exception {
+	  
+    server.enqueue(new MockResponse().setBody("retry!"));
+    server.enqueue(new MockResponse().setBody("success!"));
+
+    LargeStreamTestInterface api = createRetryableFeignBuilder()
+        	.target( LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+    
+    byte[] requestData = new byte[16184];
+	new Random().nextBytes(requestData);
+    
+	// if we wrap the inputstream in a reset limited inputstream like a BufferedInputStream, retry will fail
+	// As of now, I don't see a way to tell the server to read *some* of the request and then fail, so we aren't getting good test coverage of the partial request reset scenario
+    FeignException e = assertThrows(FeignException.class, () -> api.postLargeStream(new BufferedInputStream(new ByteArrayInputStream(requestData), 1024)) );
+    
+    assertThat(e).hasCauseInstanceOf(IOException.class);
+    
+  }    
+  
   @Test
   void streamingFileRequest(@TempDir Path tempPath) throws Exception{
 	    server.enqueue(new MockResponse());
@@ -1338,7 +1403,7 @@ public class FeignTest {
   interface LargeStreamTestInterface {
 
 	    @RequestLine("POST /")
-	    void postLargeStream(InputStream stream);
+	    String postLargeStream(InputStream stream);
 
 	    @RequestLine("POST /")
 	    void postLargeFile(File file);
