d00312cc2a8747965909a4cc4b2f24c708f9cbdb
kevin <kevin@trumpetinc.com>
2026-07-27 15:09:29 -0700

    Add PredicateEncoder and supporting classes

---
+++ api/src/main/java/feign/codec/EncoderPredicate.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2012 The Feign Authors (feign@commonhaus.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feign.codec;
+
+import java.lang.reflect.Type;
+
+import feign.RequestTemplate;
+
+/** A predicate that determines whether a given object can be encoded by an encoder. */
+@FunctionalInterface
+public interface EncoderPredicate {
+
+  /**
+   * Tests whether the given object can be encoded by an encoder.
+   *
+   * @param object the object to be encoded
+   * @param bodyType the type of the object to be encoded
+   * @param template the request template that will be used to encode the object
+   * @return {@code true} if the object can be encoded, {@code false} otherwise
+   */
+  boolean test(Object object, Type bodyType, RequestTemplate template);
+}
---
+++ core/src/test/java/feign/core/codec/PredicateEncoderTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2012 The Feign Authors (feign@commonhaus.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feign.core.codec;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import feign.RequestTemplate;
+import feign.codec.Encoder;
+import feign.codec.EncoderPredicate;
+
+@ExtendWith(MockitoExtension.class)
+class PredicateEncoderTest {
+	
+  String object = "Hello, World!";
+  Class<String> bodyType = String.class;
+  RequestTemplate requestTemplate = mock(RequestTemplate.class);
+	
+	
+  @Mock private Encoder delegate;
+  private final EncoderPredicate truePredicate = (o, t, tt) -> true;
+  private final EncoderPredicate falsePredicate = (o, t, tt) -> false;
+  
+  @Test
+  void shouldEncode() {
+
+	when(delegate.encode(object, bodyType, requestTemplate)).thenReturn(true);
+	
+    PredicateEncoder predicateEncoder = new PredicateEncoder(delegate, truePredicate);
+    
+    assertThat(predicateEncoder.encode(object, bodyType, requestTemplate)).isTrue();
+
+    verify(delegate).encode(object, bodyType, requestTemplate);
+  }
+  
+  @Test
+  void shouldNotEncode() {
+	PredicateEncoder predicateEncoder = new PredicateEncoder(delegate, falsePredicate);
+
+	assertThat(predicateEncoder.encode(object, bodyType, requestTemplate)).isFalse();
+
+	verify(delegate, never()).encode(object, bodyType, requestTemplate);
+
+  }
+
+
+
+}
