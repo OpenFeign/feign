f6750bc0d2b5ecbc40b5219fd6d1283e7ac090e2
kevin <kevin@trumpetinc.com>
2026-09-08 11:01:51 -0600

    Make sure mock web server gets closed

--- core/src/test/java/feign/core/codec/InputStreamAndReaderDecoderTest.java
+++ core/src/test/java/feign/core/codec/InputStreamAndReaderDecoderTest.java
@@ -31,7 +31,6 @@ import okio.Buffer;
 import org.junit.jupiter.api.Test;
 
 public class InputStreamAndReaderDecoderTest {
-  public final MockWebServer server = new MockWebServer();
 
   interface LargeStreamTestInterface {
 
@@ -45,88 +44,96 @@ public class InputStreamAndReaderDecoderTest {
   @Test
   void streamingResponse() throws Exception {
 
-    server.start();
+    try (MockWebServer server = new MockWebServer()) {
 
-    byte[] expectedResponse = new byte[16184];
-    new Random().nextBytes(expectedResponse);
-    server.enqueue(
-        new MockResponse.Builder()
-            .body(new BufferMockResponseBody(new Buffer().write(expectedResponse)))
-            .build());
+      server.start();
 
-    LargeStreamTestInterface api =
-        Feign.builder()
-            .decoder(new InputStreamAndReaderDecoder())
-            .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+      byte[] expectedResponse = new byte[16184];
+      new Random().nextBytes(expectedResponse);
+      server.enqueue(
+          new MockResponse.Builder()
+              .body(new BufferMockResponseBody(new Buffer().write(expectedResponse)))
+              .build());
 
-    try (InputStream is = api.getLargeStream()) {
-      byte[] out = is.readAllBytes();
-      assertThat(out.length).isEqualTo(expectedResponse.length);
-      assertThat(out).isEqualTo(expectedResponse);
+      LargeStreamTestInterface api =
+          Feign.builder()
+              .decoder(new InputStreamAndReaderDecoder())
+              .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+
+      try (InputStream is = api.getLargeStream()) {
+        byte[] out = is.readAllBytes();
+        assertThat(out.length).isEqualTo(expectedResponse.length);
+        assertThat(out).isEqualTo(expectedResponse);
+      }
     }
   }
 
   @Test
   void streamingReaderResponse() throws Exception {
+    try (MockWebServer server = new MockWebServer()) {
 
-    server.start();
+      server.start();
 
-    String expectedResponse =
-        new Random()
-            .ints(1, 1500 + 1)
-            .limit(16184)
-            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-            .toString();
+      String expectedResponse =
+          new Random()
+              .ints(1, 1500 + 1)
+              .limit(16184)
+              .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+              .toString();
 
-    server.enqueue(
-        new MockResponse.Builder()
-            .body(
-                new BufferMockResponseBody(
-                    new Buffer().write(expectedResponse.getBytes(StandardCharsets.UTF_16))))
-            .addHeader("content-type", "text/plan; charset=utf-16")
-            .build());
+      server.enqueue(
+          new MockResponse.Builder()
+              .body(
+                  new BufferMockResponseBody(
+                      new Buffer().write(expectedResponse.getBytes(StandardCharsets.UTF_16))))
+              .addHeader("content-type", "text/plan; charset=utf-16")
+              .build());
 
-    LargeStreamTestInterface api =
-        Feign.builder()
-            .decoder(new InputStreamAndReaderDecoder())
-            .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+      LargeStreamTestInterface api =
+          Feign.builder()
+              .decoder(new InputStreamAndReaderDecoder())
+              .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
 
-    try (Reader r = api.getLargeReader()) {
-      String out = Util.toString(r);
-      assertThat(out.length()).isEqualTo(expectedResponse.length());
-      assertThat(out).isEqualTo(expectedResponse);
+      try (Reader r = api.getLargeReader()) {
+        String out = Util.toString(r);
+        assertThat(out.length()).isEqualTo(expectedResponse.length());
+        assertThat(out).isEqualTo(expectedResponse);
+      }
     }
   }
 
   @Test
   void streamingReaderResponseWithNoCharset() throws Exception {
 
-    server.start();
+    try (MockWebServer server = new MockWebServer()) {
 
-    String expectedResponse =
-        new Random()
-            .ints(1, 1500 + 1)
-            .limit(16184)
-            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-            .toString();
+      server.start();
 
-    server.enqueue(
-        new MockResponse.Builder()
-            .body(
-                new BufferMockResponseBody(
-                    new Buffer().write(expectedResponse.getBytes(Util.UTF_8))))
-            .addHeader("content-type", "text/plan")
-            .build());
+      String expectedResponse =
+          new Random()
+              .ints(1, 1500 + 1)
+              .limit(16184)
+              .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+              .toString();
 
-    LargeStreamTestInterface api =
-        Feign.builder()
-            .decoder(new InputStreamAndReaderDecoder())
-            .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+      server.enqueue(
+          new MockResponse.Builder()
+              .body(
+                  new BufferMockResponseBody(
+                      new Buffer().write(expectedResponse.getBytes(Util.UTF_8))))
+              .addHeader("content-type", "text/plan")
+              .build());
 
-    try (Reader r = api.getLargeReader()) {
-      String out = Util.toString(r);
-      assertThat(out.length()).isEqualTo(expectedResponse.length());
-      assertThat(out).isEqualTo(expectedResponse);
+      LargeStreamTestInterface api =
+          Feign.builder()
+              .decoder(new InputStreamAndReaderDecoder())
+              .target(LargeStreamTestInterface.class, "http://localhost:" + server.getPort());
+
+      try (Reader r = api.getLargeReader()) {
+        String out = Util.toString(r);
+        assertThat(out.length()).isEqualTo(expectedResponse.length());
+        assertThat(out).isEqualTo(expectedResponse);
+      }
     }
   }
 }
