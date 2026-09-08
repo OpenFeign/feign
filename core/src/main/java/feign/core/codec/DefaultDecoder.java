6702ab5fd016d9cd31dc162844513bf1a46de351
kevin <kevin@trumpetinc.com>
2026-09-08 16:20:20 -0600

    Kill accidental diff

--- core/src/main/java/feign/core/codec/DefaultDecoder.java
+++ core/src/main/java/feign/core/codec/DefaultDecoder.java
@@ -29,7 +29,6 @@ public class DefaultDecoder extends StringDecoder {
     if (byte[].class.equals(type)) {
       return Util.toByteArray(response.body().asInputStream());
     }
-
     return super.decode(response, type);
   }
 }
