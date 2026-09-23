b3dd854bc4d14c63d198ed5e70d15202e889b4eb
kevin <kevin@trumpetinc.com>
2026-09-23 07:29:06 -0600

    Add javadoc

--- core/src/main/java/feign/core/codec/InputStreamAndReaderDecoder.java
+++ core/src/main/java/feign/core/codec/InputStreamAndReaderDecoder.java
@@ -26,6 +26,12 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.lang.reflect.Type;
 
+/**
+ * A decoder that converts response bodies into {@code InputStream} and {@code Reader} response types.
+ *
+ * <p><b>Important:</b> the caller of the Feign interface template method is responsible for closing
+ * the returned stream.
+ */
 public class InputStreamAndReaderDecoder implements PredicatedDecoder {
 
   @Override
